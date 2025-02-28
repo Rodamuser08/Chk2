673fa7add2558909f2864dc5c18276c513957fd9
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-02-28 12:15:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import telebot,time
 from telebot import types
 from gatet import Tele
 import os
-token = '7846937912:AAGIYkT98aqcczx3xHontR7Nct0IaZWM3EA'
+token = '7040296782:AAFXynxC35O099GeYNeXjUTrJsJ0a4QB_Qs'
 bot=telebot.TeleBot(token,parse_mode="HTML")
 @bot.message_handler(commands=["start"])
 def start(message):
@@ -90,7 +90,7 @@ by ➜ @strawhatchannel96 ''', reply_markup=mes)
 𝐓𝐢𝐦𝐞: <code>1{"{:.1f}".format(execution_time)} second</code> 
 𝐁𝐨𝐭 𝐀𝐛𝐨𝐮𝐭: @strawhatchannel96'''
 				print(last)
-				if 'succeeded' in last:
+				if 'thank' in last:
 					ch += 1
 					bot.reply_to(message, msg)
 					
