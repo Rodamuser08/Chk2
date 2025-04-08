e93c4e4139d2e3f07063dad7dece8b0aa34beab0
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-04-08 09:41:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import telebot,time
 from telebot import types
 from gatet import Tele
 import os
-token = '7040296782:AAFXynxC35O099GeYNeXjUTrJsJ0a4QB_Qs'
+token = '8181812070:AAE730BqIxuKpmikhYLKBawtSYZay5UaUts'
 bot=telebot.TeleBot(token,parse_mode="HTML")
 @bot.message_handler(commands=["start"])
 def start(message):
