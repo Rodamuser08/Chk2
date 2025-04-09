26b9450c9aaf2748b0d433ae9f97c24691321f93
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-04-09 12:17:17 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -26,29 +26,27 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F2710656340%3B+stripe-js-v3%2F2710656340%3B+card-element&key=pk_live_51Jez78BKwt72jNCmXW07XYSzt9cnaUVtC9aTdAF3ImhhQ8KcGKRlpJh9VjBEqq51laaZcYO4GWHpHRU8nCSP5WAB00I9qJSMtV'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F0c7ffc14a8%3B+stripe-js-v3%2F0c7ffc14a8%3B+card-element&key=pk_live_51GNVZkCmff2HpFXWPLIJkfTlT6Su7Eq6gWYmMfHkrZQpfruXHDBKHaELfzSnhUQCfrGi1pQBwAGZtRXiv9KarRCj00PW04obKr'
 	
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    '_ga': 'GA1.2.1574929798.1740392008',
-	    '__stripe_mid': 'dad89c44-abb0-4281-9cdc-44510dd11f1fc4fc70',
-	    'PHPSESSID': '4svf2fepb9mbuv5rbtac9khh8l',
-	    '_gid': 'GA1.2.1348120377.1744079191',
-	    '_ga_Y6Z6HMHW5Q': 'GS1.2.1744079191.9.0.1744079191.0.0.0',
-	    '__stripe_sid': 'cfef4806-df27-4e7c-9913-217e0d6b17306828df',
+	    '_ga': 'GA1.1.867590447.1744175270',
+	    '__stripe_mid': '08136e65-9524-4895-b8df-61816985366e667d09',
+	    '__stripe_sid': '011f83ac-0a73-4a4c-bb33-c3c0f23d9e5d9ecd34',
+	    '_ga_GJL9GQNRTP': 'GS1.1.1744175270.1.1.1744175293.0.0.0',
 	}
 	
 	headers = {
-	    'authority': 'www.sussexswimming.org',
+	    'authority': 'thehop-e.com',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': '_ga=GA1.2.1574929798.1740392008; __stripe_mid=dad89c44-abb0-4281-9cdc-44510dd11f1fc4fc70; PHPSESSID=4svf2fepb9mbuv5rbtac9khh8l; _gid=GA1.2.1348120377.1744079191; _ga_Y6Z6HMHW5Q=GS1.2.1744079191.9.0.1744079191.0.0.0; __stripe_sid=cfef4806-df27-4e7c-9913-217e0d6b17306828df',
-	    'origin': 'https://www.sussexswimming.org',
-	    'referer': 'https://www.sussexswimming.org/swimming/county-championships-2025/coach-team-manager-lunch-orders-championships-2025/',
+	    # 'cookie': '_ga=GA1.1.867590447.1744175270; __stripe_mid=08136e65-9524-4895-b8df-61816985366e667d09; __stripe_sid=011f83ac-0a73-4a4c-bb33-c3c0f23d9e5d9ecd34; _ga_GJL9GQNRTP=GS1.1.1744175270.1.1.1744175293.0.0.0',
+	    'origin': 'https://thehop-e.com',
+	    'referer': 'https://thehop-e.com/breathe-thru-grief-conference/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -60,21 +58,15 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1744079268123',
+	    't': '1744175336019',
 	}
 	
 	data = {
-	    'data': '__fluent_form_embded_post_id=14688&_fluentform_30_fluentformnonce=df5e01d7e6&_wp_http_referer=%2Fswimming%2Fcounty-championships-2025%2Fcoach-team-manager-lunch-orders-championships-2025%2F&input_text_1=Rodam&input_text_2=User&email=rodamuser08%40gmail.com&multi_select%5B%5D=Worthing%20SC&input_radio_sat10=Saturday%208thFebruary%20option%201%20&custom-payment-amount=0.30&payment_method=stripe&input_radio_sat11=&input_radio_sat24=&input_radio_sat25=&__stripe_payment_method_id='+str(pm)+'',
+	    'data': '__fluent_form_embded_post_id=6143&_fluentform_7_fluentformnonce=a357c8786e&_wp_http_referer=%2Fbreathe-thru-grief-conference%2F&name=Rodam%20User&address%5Baddress_line_1%5D=Street%2027&address%5Bcity%5D=New%20York&address%5Bstate%5D=New%20York&address%5Bzip%5D=10080&phone_number=0817480671&email_address=rodamuser08%40gmail.com&amount=1.00&payment_method=stripe&__stripe_payment_method_id='+str(pm)+'',
 	    'action': 'fluentform_submit',
-	    'form_id': '30',
+	    'form_id': '7',
 	}
 	
-	r2 = requests.post(
-	    'https://www.sussexswimming.org/wp-admin/admin-ajax.php',
-	    params=params,
-	    cookies=cookies,
-	    headers=headers,
-	    data=data,
-	)
+	r2 = requests.post('https://thehop-e.com/wp-admin/admin-ajax.php', params=params, cookies=cookies, headers=headers, data=data)
 	
 	return (r2.json())
