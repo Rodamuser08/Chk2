e1495ad2b204d593a4ea9f8c6e55aaff2b384c0a
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-04-20 16:33:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ by ➜ @strawhatchannel96 ''', reply_markup=mes)
 𝐓𝐢𝐦𝐞: <code>1{"{:.1f}".format(execution_time)} second</code> 
 𝐁𝐨𝐭 𝐀𝐛𝐨𝐮𝐭: @strawhatchannel96'''
 				print(last)
-				if 'Thank' in last:
+				if 'premium' in last:
 					ch += 1
 					bot.reply_to(message, msg)
 					
