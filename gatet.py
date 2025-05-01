fefe55d62cf2884d7458f9e48acf60b9b785a1e3
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-05-02 06:55:06 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -10,47 +10,47 @@ def Tele(ccx):
 		yy = yy.split("20")[1]
 	r = requests.session()
 
+		headers = {
+	    'authority': 'www.penfold.com.au',
+	    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'referer': 'https://www.google.com/',
+	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'document',
+	    'sec-fetch-mode': 'navigate',
+	    'sec-fetch-site': 'same-origin',
+	    'sec-fetch-user': '?1',
+	    'upgrade-insecure-requests': '1',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
+	}
+	
+	response = requests.get('https://www.penfold.com.au/online-payment', headers=headers)
+	
+	merchant_id = re.search(r'\\"merchant_id\\":\\"(.*?)\\"', response.text).group(1)
+	
 	headers = {
-        'authority': 'www.penfold.com.au',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-        'referer': 'https://www.google.com/',
-        'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
-        'sec-ch-ua-mobile': '?1',
-        'sec-ch-ua-platform': '"Android"',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
-    }
-    
-    response = requests.get('https://www.penfold.com.au/online-payment', headers=headers)
-    
-    merchant_id = re.search(r'\\"merchant_id\\":\\"(.*?)\\"', response.text).group(1)
-    
-    headers = {
-        'authority': 'www.penfold.com.au',
-        'accept': 'application/json',
-        'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-        'content-type': 'text/plain;charset=UTF-8',
-        'origin': 'https://www.penfold.com.au',
-        'referer': 'https://www.penfold.com.au/online-payment',
-        'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
-        'sec-ch-ua-mobile': '?1',
-        'sec-ch-ua-platform': '"Android"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
-    }
-    
-    data = '{"lead":{"dealership_slug":"559","website_slug":"penfold","url":"https://www.penfold.com.au/online-payment","source":"Website","user_agent":"Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36","invoice_reference":"1","name":"Rodam User","amount":"10","email":"rodamuser08@gmail.com","phone":"6148 008 945","leadStatus":"Incomplete","form_completed":false,"category":"General Enquiry","subcategory":"Incomplete Payment","visited_pages":["/online-payment"],"linked_query_params":{}}}'
-    
-    response = requests.post('https://www.penfold.com.au/api/leads', headers=headers, data=data)
-    
-    id = response.json()['id']
+	    'authority': 'www.penfold.com.au',
+	    'accept': 'application/json',
+	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'content-type': 'text/plain;charset=UTF-8',
+	    'origin': 'https://www.penfold.com.au',
+	    'referer': 'https://www.penfold.com.au/online-payment',
+	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'empty',
+	    'sec-fetch-mode': 'cors',
+	    'sec-fetch-site': 'same-origin',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
+	}
+	
+	data = '{"lead":{"dealership_slug":"559","website_slug":"penfold","url":"https://www.penfold.com.au/online-payment","source":"Website","user_agent":"Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36","invoice_reference":"1","name":"Rodam User","amount":"10","email":"rodamuser08@gmail.com","phone":"6148 008 945","leadStatus":"Incomplete","form_completed":false,"category":"General Enquiry","subcategory":"Incomplete Payment","visited_pages":["/online-payment"],"linked_query_params":{}}}'
+	
+	response = requests.post('https://www.penfold.com.au/api/leads', headers=headers, data=data)
+	
+	id = response.json()['id']
 	
 	headers = {
 	    'authority': 'api.payway.com.au',
