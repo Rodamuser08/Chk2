2c5252955f2639b1679f39406c7621bad021177d
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-05-04 03:19:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ by ➜ @strawhatchannel96 ''', reply_markup=mes)
 𝐓𝐢𝐦𝐞: <code>1{"{:.1f}".format(execution_time)} second</code> 
 𝐁𝐨𝐭 𝐀𝐛𝐨𝐮𝐭: @strawhatchannel96'''
 				print(last)
-				if '' in last:
+				if 'Thank' in last:
 					ch += 1
 					bot.reply_to(message, msg)
 					
@@ -108,6 +108,7 @@ by ➜ @strawhatchannel96 ''', reply_markup=mes)
 				    	
 				else:
 					dd += 1
+					time.sleep(5)
 	except Exception as e:
 		print(e)
 	bot.edit_message_text(chat_id=message.chat.id, message_id=ko, text='CHECKED ✅\nBOT BY ➜ @strawhatchannel96')
