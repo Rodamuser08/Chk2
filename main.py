9dfe70e5f31f0b0aaa04c4c6f65105039626069f
Daokhaosaard13 <daokhaosaard13@gmail.com>
2025-05-11 15:01:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ by ➜ @strawhatchannel96 ''', reply_markup=mes)
 𝐁𝐨𝐭 𝐀𝐛𝐨𝐮𝐭: @strawhatchannel96'''
 				
 				print(last)
-				if 'successfully' in last:
+				if 'Thank' in last:
 					ch += 1
 					bot.reply_to(message, msg)
 					
