f8ef4e1f569d051190f0c53c94b964f84c1bc86e
Rodamuser08 <rodamuser08@gmail.com>
2025-05-12 01:00:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ by ➜ @strawhatchannel96 ''', reply_markup=mes)
 𝐁𝐨𝐭 𝐀𝐛𝐨𝐮𝐭: @strawhatchannel96'''
 				
 				print(last)
-				if 'PURCHA' in last:
+				if 'PURCHASE' in last:
 					ch += 1
 					bot.reply_to(message, msg)
 					
