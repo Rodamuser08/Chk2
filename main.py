47da91aebb1a4a45c104905fc1d9632597f6a1c4
Rodamuser08 <rodamuser08@gmail.com>
2025-05-14 02:43:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ by ➜ @strawhatchannel96 ''', reply_markup=mes)
 𝐁𝐨𝐭 𝐀𝐛𝐨𝐮𝐭: @strawhatchannel96'''
 				
 				print(last)
-				if 'Payment ' in last:
+				if 'Payment Confirmation' in last:
 					ch += 1
 					bot.reply_to(message, msg)
 					
